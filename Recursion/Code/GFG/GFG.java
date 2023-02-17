b625e281e5716e5aa3d52f5d507e7d5b0a45c5eb
Ahmadjajja <ahmadjajja86@gmail.com>
2023-02-17 09:15:45 +0500

    Recursion implementation

---
+++ Recursion/Code/GFG/GFG.java
@@ -0,0 +1,28 @@
+package Recursion;
+// package Recursion;
+
+// A Java program to demonstrate working of
+// recursion
+class GFG {
+    static void printFun(int test)
+    {
+        if (test < 1)
+            return;
+        else {
+            System.out.printf("\n%d ", test);
+            printFun(test - 1); // statement 2
+            if (test == 1) {
+                System.out.println("\nAfter base condition is met");
+            }
+            System.out.printf("\n%d ", test);
+            return;
+        }
+    }
+ 
+    // Driver Code
+    public static void main(String[] args)
+    {
+        int test = 3;
+        printFun(test);
+    }
+}
