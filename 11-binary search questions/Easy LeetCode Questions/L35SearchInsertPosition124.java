fa425ced5d926d459938d2aa33bc9e1a5804c667
Ahmadjajja <ahmadjajja86@gmail.com>
2023-03-29 19:37:06 +0500

    Day-25 Java_Problem_No_124 solved

---
+++ 11-binary search questions/Easy LeetCode Questions/L35SearchInsertPosition124.java
@@ -0,0 +1,24 @@
+// Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+
+// You must write an algorithm with O(log n) runtime complexity.
+
+public class L35SearchInsertPosition124 {
+    public static int searchInsert(int[] nums, int target) {
+        int start = 0, end = nums.length - 1;
+        while(start <= end){
+            int mid = start + (end - start) / 2;
+            if(nums[mid] == target){
+                return mid;
+            }
+            if(nums[mid] > target){
+                end = mid -1;
+            }else {
+                start = mid + 1;
+            }
+        }
+        return start;
+    }
+    public static void main(String[] args) {
+        System.out.println(searchInsert(new int[]{1,3,5,6}, 7));
+    }
+}
