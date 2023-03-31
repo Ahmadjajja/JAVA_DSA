d5e00bec73e9f5e3e03058d42456140df968f65a
Ahmadjajja <ahmadjajja86@gmail.com>
2023-03-31 22:22:04 +0500

    Day-27 Java_Problem_No_135 solved

---
+++ 11-binary search questions/Medium LeetCode Questions/L33SearchInRotatedSortedArray135.java
@@ -0,0 +1,38 @@
+// There is an integer array nums sorted in ascending order (with distinct values).
+
+// Prior to being passed to your function, nums is possibly rotated at an unknown pivot index k (1 <= k < nums.length) such that the resulting array is [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]] (0-indexed). For example, [0,1,2,4,5,6,7] might be rotated at pivot index 3 and become [4,5,6,7,0,1,2].
+
+// Given the array nums after the possible rotation and an integer target, return the index of target if it is in nums, or -1 if it is not in nums.
+
+// You must write an algorithm with O(log n) runtime complexity.
+
+public class L33SearchInRotatedSortedArray135 {
+    public static int search(int[] nums, int target) {
+        int start = 0, end = nums.length - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            }
+            // Left portion
+            if (nums[start] <= nums[mid]) {
+                if (target > nums[mid] || target < nums[start]) {
+                    start = mid + 1;
+                } else {
+                    end = mid - 1;
+                }
+            } else { // Right portion
+                if (target < nums[mid] || target > nums[end]) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(search(new int[] { 2, 3, 4, 5, 6, 7, 8, 9, 1 }, 9));
+    }
+}
--- 11-binary search questions/Medium LeetCode Questions/ReadMe.md
+++ 11-binary search questions/Medium LeetCode Questions/ReadMe.md
@@ -3,3 +3,5 @@
 **(133) Question 01** <a href="https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/" target="_blank" style="font-size: 16px;dispaly:inline-block;">_34. Find First and Last Position of Element in Sorted Array_</a> <br/>
  
 **(134) Question 02** <a href="https://leetcode.com/problems/single-element-in-a-sorted-array/submissions/" target="_blank" style="font-size: 16px;dispaly:inline-block;">_540. Single Element in a Sorted Array_</a> <br/>
+ 
+**(135) Question 03** <a href="https://leetcode.com/problems/search-in-rotated-sorted-array/submissions/" target="_blank" style="font-size: 16px;dispaly:inline-block;">_33. Search in Rotated Sorted Array_</a> <br/>
