f73ff882f8a9eb7a4909fdfac10ed40ce670ff48
Ahmadjajja <ahmadjajja86@gmail.com>
2023-04-16 12:01:07 +0500

    Day-37 Merge Sort using recursion

---
+++ 12-sorting/code/MergeSort.java
@@ -0,0 +1,69 @@
+import java.util.Arrays;
+
+import java.util.Arrays;
+
+public class MergeSort {
+
+    public static void main(String[] args) {
+        int[] arr = {5, 4, 3, 2, 1};
+        arr = mergeSort(arr);
+        
+        // Things to note over here is that recursion actually creating new objects for every function call, Original array is not been modified
+        System.out.println(Arrays.toString(arr));
+    }
+
+    public static int[] mergeSort(int[] arr) {
+        if (arr.length == 1) {
+            return arr;
+        }
+
+        int mid = arr.length / 2;
+
+        // Things to note over here is that recursion actually
+        // creating new objects for every function call, Original
+        // array is not been modified
+
+        int[] left = mergeSort(Arrays.copyOfRange(arr, 0, mid));
+        int[] right = mergeSort(Arrays.copyOfRange(arr, mid, arr.length));
+
+        return merge(left, right);
+    }
+
+    private static int[] merge(int[] first, int[] second) {
+        int[] mix = new int[first.length + second.length];
+
+        int i = 0; // this pointer points to first array
+        int j = 0; // this pointer points to second array
+        int k = 0; // this pointer points to mix array
+
+        while (i < first.length && j < second.length) {
+            if (first[i] < second[j]) {
+                mix[k] = first[i];
+                i++;
+            } else {
+                mix[k] = second[j];
+                j++; 
+            }
+            k++;
+        }
+
+        // it may be possible that one of the arrays is not complete
+        // copy the remaining elements
+        // in both of these loops either one is going to true
+        while(i < first.length){
+            mix[k] = first[i];
+            i++;
+            k++;
+        }
+
+        while(j < second.length){
+            mix[k] = second[j];
+            j++;
+            k++;
+        }
+
+        return mix;
+    }
+}
+
+//
---
+++ 12-sorting/code/quickSort.java
@@ -0,0 +1,5 @@
+public class quickSort {
+    public static void main(String[] args) {
+        
+    }
+}
