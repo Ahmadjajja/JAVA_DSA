6e5f569235b43c0baadf5204796d281af02fa35b
Ahmadjajja <ahmadjajja86@gmail.com>
2023-04-29 16:04:02 +0500

    Day-46 Java_Problem_No 221 Solved

--- 15-strings questions/Medium LeetCode Questions/ReadMe.md
+++ 15-strings questions/Medium LeetCode Questions/ReadMe.md
@@ -2,3 +2,5 @@
 
 **(220) Question 01** <a href="https://leetcode.com/problems/jump-game-vii/submissions/941443533/" target="_blank" style="font-size: 16px;dispaly:inline-block;">_1871. Jump Game VII_</a> <br/>
 
+**(221) Question 02** <a href="https://leetcode.com/problems/split-two-strings-to-make-palindrome/submissions/941502237/" target="_blank" style="font-size: 16px;dispaly:inline-block;">_1616. Split Two Strings to Make Palindrome_</a> <br/>
+
--- 15-strings questions/Medium LeetCode Questions/Testing.java
+++ 15-strings questions/Medium LeetCode Questions/Testing.java
@@ -1,40 +1,42 @@
 import java.util.Arrays;
 
 class Testing {
-    public String defangIPaddr(String address) {
-        StringBuilder add = new StringBuilder();
-        add.append("");
-        for(int i = 0; i < address.length(); i++){
-            if(address.charAt(i) == '.'){
-                add.append("[.]");
-                continue;
+    public static String defangIPaddr(String address) {
+        System.out.println("palindrome(rar) : " + palindrome("rars"));
+        return "";
+    }
+    private static boolean palindrome(String s) {
+        int start = 0, end = s.length() - 1;
+        while (start < end) {
+            if (s.charAt(start) != s.charAt(end)) {
+                return false;
             }
-            add.append(address.charAt(i));
+            start++;
+            end--;
         }
-        address.replace('0', '0');
-        return add.toString();
+        return true;
     }
     public static void main(String[] args) {
+        defangIPaddr("Ahmad");
         // System.out.println("hey");
-        StringBuilder str = new StringBuilder("Ahmad");
+        // StringBuilder str = new StringBuilder("Ahmad");
         // System.out.println(str);
         // str = new StringBuilder("jajja");
         // System.out.println(str);
 
         // System.out.println(Character.getNumericValue('#'));
         // System.out.println();
-        System.out.println(Arrays.toString(new String[]{}).equals(Arrays.toString(new String[]{"1"})));
-        String name = "ahmad";
+        // System.out.println(Arrays.toString(new String[]{}).equals(Arrays.toString(new String[]{"1"})));
+        // String name = "ahmad";
+        // System.out.println("ahmad".substring(5, 5));
 
         
-        System.out.println(name.indexOf('h'));
+        // System.out.println(name.indexOf('h'));
         
-        for (String string : args) {
+        // for (String string : args) {
             
-        }
+        // }
         
     }
-    private static char[] Char(int i) {
-        return null;
-    }
+
 }
