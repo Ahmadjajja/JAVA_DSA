a05b9be0923db27526ea17e4a2e1c1fdc3d9dd41
Ahmadjajja <ahmadjajja86@gmail.com>
2023-05-13 11:08:23 +0500

    Day-54 pettern questions 24 solved

--- 17-patterns questions/Pattern24.java
+++ 17-patterns questions/Pattern24.java
@@ -16,11 +16,37 @@ public class Pattern24 {
     }
 
     static void pattern(int n) {
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= i; j++) {
-                System.out.print("* ");
+        int asterisks = 0, midSpace = 0, innerSpace = 0, outerSpace = 0, totalCols = 2*n;
+        for (int rows = 1; rows <= 2*n; rows++) {
+            if(rows == 1 || rows == 2*n){
+                asterisks = 2;
+                midSpace = totalCols - asterisks;
+                for (int i = 1; i <= asterisks; i++) {
+                    System.out.print("*");
+                    for (int index = 0; index < midSpace && i != asterisks; index++) {
+                        System.out.print(" ");
+                    }
+                }
+                System.out.println();
+            } else {
+                asterisks = 4;
+                innerSpace = rows <= n ? rows - 2: totalCols - rows - 1;
+                outerSpace = totalCols - 4 - 2*innerSpace;
+
+                for (int i = 1; i <= asterisks; i++) {
+                    System.out.print("*");
+                    if (i == 1 || i == 3) {
+                        for (int j = 0; j < innerSpace; j++) {
+                            System.out.print(" ");
+                        }
+                    } else if(i == 2){
+                        for (int index = 0; index < outerSpace; index++) {
+                            System.out.print(" ");
+                        }
+                    }
+                }
+                System.out.println();
             }
-            System.out.println();
         }
     }
 }
