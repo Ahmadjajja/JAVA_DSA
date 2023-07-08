37597e019a19c2e15c8aca3f6dbb570808177704
Ahmad jajja <ahmadjajja86@gmail.com>
2023-07-08 10:38:30 +0500

    Day-70  factors

--- 22-math/Code/Factors.java
+++ 22-math/Code/Factors.java
@@ -7,7 +7,7 @@ public class Factors {
         for (int i = 1; i <= n; i++) {
             if (n % i == 0) {
                 System.out.print(i + " ");
-            }
+            } 
         }
     }
 }
