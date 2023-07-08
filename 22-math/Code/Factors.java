1773cd241bb60c18560f194065ecb4f7fa8e7993
Ahmad jajja <ahmadjajja86@gmail.com>
2023-07-08 10:37:52 +0500

    Day-68  factors

---
+++ 22-math/Code/Factors.java
@@ -0,0 +1,13 @@
+public class Factors {
+    public static void main(String[] args) {
+        int n = 20;
+        factors(n);
+    }
+    static void factors(int n){
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
