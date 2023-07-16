d847592d68d7254f3f9fdcbb5ebaf6d0d3677e43
Ahmad Jajja <86593662+Ahmadjajja@users.noreply.github.com>
2023-07-16 13:14:39 -0700

    Time: 55 ms (76.25%) | Memory: 41.8 MB (50.66%) - LeetSync

---
+++ DailyLeetcoding/2813-to-be-or-not-to-be/to-be-or-not-to-be.js
@@ -0,0 +1,26 @@
+/**
+ * @param {string} val
+ * @return {Object}
+ */
+var expect = function(val) {
+    return {
+        toBe: (val2) => {
+            if (val !== val2) throw new Error("Not Equal");
+            else return true;
+        },
+        notToBe: (val2) => {
+            if (val === val2) throw new Error("Equal");
+            else return true;
+        }
+    }
+};
+
+/* For example, when expect(5).toBe(4) is called,
+   val is the expect parameter (so val equals 5),
+   val2 is the toBe parameter (so val2 equals 4).
+   Since val !== val2, aka 5 != 4, we throw error "Not Equal". */
+
+/**
+ * expect(5).toBe(5); // true
+ * expect(5).notToBe(5); // throws "Equal"
+ */
