801b1a01606635a38a7c647ed4a59e380bdc020b
Ahmad jajja <ahmadjajja86@gmail.com>
2023-07-19 15:53:10 +0500

    Day-82 Recursion Pattern Questions

--- 18-recursion/code/patterns/Triangle.java
+++ 18-recursion/code/patterns/Triangle.java
@@ -1,78 +1,75 @@
-package com.kunal.patterns;
-
 import java.util.Arrays;
 
 public class Triangle {
     public static void main(String[] args) {
-//        triangle2(4, 0);
-        int[] arr = {1, 4, 3, 5};
-        selection(arr, arr.length, 0, 0);
+        // triangle(4, 0);
+        // triangle2(4, 0);
+        int[] arr = {4, 3, 2, 8, 10};
+        // bubble(arr, arr.length - 1, 0);
+        selection(arr, arr.length - 1, 0, Integer.MIN_VALUE, -1);
+        System.out.println();
         System.out.println(Arrays.toString(arr));
     }
-
-    static void triangle2(int r, int c) {
-        if (r == 0) {
+// * * * *  (This concept is like: Bubble Sort & Selection Sort)
+// * * * 
+// * *
+// *
+    static void triangle(int r, int c){
+         if (r == 0) {
             return;
-        }
-        if (c < r) {
-            triangle2(r, c+1);
-            System.out.print("*");
-        } else {
-            triangle2(r-1, 0);
-            System.out.println();
-        }
+         }
+         if (c < r) {
+             System.out.print("*");
+             triangle(r, c + 1);
+         } else {
+             System.out.println();
+             triangle(r - 1, 0);
+         }
     }
-
-    static void triangle(int r, int c) {
-        if (r == 0) {
+    static void triangle2(int r, int c){
+         if (r == 0) {
             return;
-        }
-        if (c < r) {
-            System.out.print("*");
-            triangle(r, c+1);
-        } else {
-            System.out.println();
-            triangle(r-1, 0);
-        }
+         }
+         if (c < r) {
+             triangle2(r, c + 1);
+             System.out.print("*");
+         } else {
+             triangle2(r - 1, 0);
+             System.out.println();
+         }
     }
-
-
-    static void bubble(int[] arr, int r, int c) {
-        if (r == 0) {
+    static void bubble(int[] arr, int r, int c){
+         if (r == 0) {
             return;
-        }
-        if (c < r) {
-
-            if (arr[c] > arr[c+1]) {
-                // swap
+         }
+         if (c < r) {
+            if (arr[c] < arr[c + 1]) {
                 int temp = arr[c];
-                arr[c] = arr[c+1];
-                arr[c+1] = temp;
+                arr[c] = arr[c + 1];
+                arr[c + 1] = temp;
             }
-
-            bubble(arr, r, c+1);
-        } else {
-            bubble(arr, r-1, 0);
-        }
+             bubble(arr, r, c + 1);
+         } else {
+             bubble(arr, r - 1, 0);
+         }
     }
-
-    static void selection(int[] arr, int r, int c, int max) {
-        if (r == 0) {
+    static void selection(int[] arr, int r, int c, int max, int maxIndex){
+         if (r == 0) {
             return;
-        }
-        if (c < r) {
-            if (arr[c] > arr[max]) {
-                selection(arr, r, c+1, c);
-            } else {
-                selection(arr, r, c+1, max);
+         }
+         if (c < r) {
+             max = Math.max(max, arr[c]);
+             if (max == arr[c]) {
+                maxIndex = c;
+             }
+             selection(arr, r, c + 1, max, maxIndex);
+         } else {
+            if (arr[maxIndex] > arr[r]) { 
+                int temp = arr[maxIndex];
+                arr[maxIndex] = arr[r];
+                arr[r] = temp;
             }
-        } else {
-            int temp = arr[max];
-            arr[max] = arr[r-1];
-            arr[r-1] = temp;
-            selection(arr, r-1, 0, 0);
-        }
+             selection(arr, r - 1, 0, Integer.MIN_VALUE, -1);
+         }
     }
-
-
 }
