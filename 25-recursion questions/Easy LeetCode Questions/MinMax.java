ac3e93fe8f6bb4eb35686cf950f91627bd873dc7
Ahmad jajja <ahmadjajja86@gmail.com>
2023-07-19 11:43:29 +0500

    Day-83 Question 331

---
+++ 25-recursion questions/Easy LeetCode Questions/MinMax.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+
+public class MinMax{
+    public static void main(String[] args) {
+        int[] arr = {1, 4, 3, -5, -4, 8, 6};
+        minMax(arr, 0);
+        System.out.println("minimum : " + min);
+        System.out.println("maximum : " + max);
+        System.out.println(minMax2(arr, 0, Integer.MAX_VALUE, Integer.MIN_VALUE));
+    }
+    static int min = Integer.MAX_VALUE;
+    static int max = Integer.MIN_VALUE;
+    private static void minMax(int[] arr, int index){
+        if(index == arr.length){
+            return;
+        }
+        min = Math.min(min, arr[index]);
+        max = Math.max(max, arr[index]);
+        minMax(arr, ++index);
+    }
+    private static ArrayList<Integer> minMax2(int[] arr, int index, int min, int max){
+        if(index == arr.length){
+            ArrayList<Integer> list = new ArrayList<>();
+            list.add(min);
+            list.add(max);
+            return list;
+        }
+        min = Math.min(min, arr[index]);
+        max = Math.max(max, arr[index]);
+        return minMax2(arr, ++index, min, max);
+    }
+}
--- 25-recursion questions/Easy LeetCode Questions/ReadMe.md
+++ 25-recursion questions/Easy LeetCode Questions/ReadMe.md
@@ -3,3 +3,5 @@
 **(329) Question 01** <a href="https://leetcode.com/problems/fibonacci-number/submissions/996606162/" target="_blank" style="font-size: 16px;dispaly:inline-block;">_509. Fibonacci Number_</a> <br/>
 
 **(330) Question 02** <a href="https://www.geeksforgeeks.org/sum-triangle-from-array/" target="_blank" style="font-size: 16px;dispaly:inline-block;">_sum-triangle-from-array (GFG)_</a> <br/>
+
+**(331) Question 03** <a href="https://www.geeksforgeeks.org/recursive-programs-to-find-minimum-and-maximum-elements-of-array/" target="_blank" style="font-size: 16px;dispaly:inline-block;">_recursive-programs-to-find-minimum-and-maximum-elements-of-array (GFG)_</a> <br/>
