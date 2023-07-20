c1ca91cda5a85e1cbedbaa9ba583304e62315a73
Ahmad jajja <ahmadjajja86@gmail.com>
2023-07-20 17:25:15 +0500

    Day-83 Recursion String subset

--- 18-recursion/code/strings/Stream.java
+++ 18-recursion/code/strings/Stream.java
@@ -2,14 +2,19 @@ package com.kunal.strings;
 
 public class Stream {
     public static void main(String[] args) {
-        System.out.println(skipAppNotApple("bacapplcdah"));
+        // skip("", "baccdahj");
+        // System.out.println(skip2("", "baccdahj"));
+        // System.out.println(skip3("baccdahj"));
+        System.out.println(skipApple("bacappledahapplej"));
+        System.out.println(skipApple2("bacappledaapphapplej"));
+        System.out.println(skipAppNotApple("bacappledahapplejapp"));
     }
-
-    static void skip(String p, String up) {
+    static void skip(String p, String up){
         if (up.isEmpty()) {
             System.out.println(p);
             return;
         }
+
         char ch = up.charAt(0);
 
         if (ch == 'a') {
@@ -18,8 +23,21 @@ public class Stream {
             skip(p + ch, up.substring(1));
         }
     }
+    static String skip2(String p, String up){
+        if (up.isEmpty()) {
+            return p;
+        }
 
-    static String skip(String up) {
+        char ch = up.charAt(0);
+
+        if (ch == 'a') {
+            return skip2(p, up.substring(1));
+        } else {
+            return skip2(p + ch, up.substring(1));
+        }
+    }
+    // Very important concept (note carefully) !!
+    static String skip3(String up){
         if (up.isEmpty()) {
             return "";
         }
@@ -27,30 +45,56 @@ public class Stream {
         char ch = up.charAt(0);
 
         if (ch == 'a') {
-            return skip(up.substring(1));
+            return skip3(up.substring(1));
         } else {
-            return ch + skip(up.substring(1));
+            // This tells, hey you are going up
+            // can you take my answer
+            return ch + skip3(up.substring(1));
         }
     }
-
-    static String skipApple(String up) {
-        if (up.isEmpty()) {
+    // Very important concept (note carefully) !!
+    static String skipApple(String up){
+        // up = bacappledahapplej
+        if (up.length() == 0) {
             return "";
         }
-        if (up.startsWith("apple")) {
+
+        String str = up.length() < 5 ? up.substring(0) : up.substring(0, 5);
+        String strChar = up.substring(0, 1);
+
+        if (str.equals("apple")) {
             return skipApple(up.substring(5));
         } else {
-            return up.charAt(0) + skipApple(up.substring(1));
+            // This tells, hey you are going up
+            // can you take my answers
+            return strChar + skipApple(up.substring(1));
         }
     }
-
-    static String skipAppNotApple(String up) {
+    static String skipApple2(String up){
+        // up = bacappledaapphapplej
         if (up.isEmpty()) {
             return "";
         }
+
+        if (up.startsWith("apple")) {
+            return skipApple2(up.substring(5));
+        } else {
+            // This tells, hey you are going up
+            // can you take my answers
+            return up.charAt(0) + skipApple2(up.substring(1));
+        }
+    }
+    static String skipAppNotApple(String up){
+        // up = bacappledahapplejapp
+        if (up.isEmpty()) {
+            return "";
+        }
+
         if (up.startsWith("app") && !up.startsWith("apple")) {
             return skipAppNotApple(up.substring(3));
         } else {
+            // This tells, hey you are going up
+            // can you take my answers
             return up.charAt(0) + skipAppNotApple(up.substring(1));
         }
     }
