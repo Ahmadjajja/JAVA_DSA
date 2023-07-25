d82448081dfe5b4b62281a8194c68282ff983b56
Ahmad Jajja <86593662+Ahmadjajja@users.noreply.github.com>
2023-07-25 10:24:25 -0700

    Time: 1 ms (100.00%) | Memory: 40 MB (29.84%) - LeetSync

--- DailyLeetcoding/342-power-of-four/power-of-four.java
+++ DailyLeetcoding/342-power-of-four/power-of-four.java
@@ -1,24 +1,44 @@
+// class Solution {
+//     public boolean isPowerOfFour(int x) {
+//         // Binary form of 4 power is as follows
+//         // 4 power 0 -> 1
+//         // 4 power 1 -> 100
+//         // 4 power 2 -> 10000     
+//         if (x<1) return false;
+//         if (x==1) return true;
+
+//         int chkbit = 3;
+
+//         // Check if the number has only one set bit
+//         if ((x & (x - 1)) != 0)
+//             return false;
+
+//         // Left-shift the number by 2 bits and check
+//         // if last two bits are zeros.
+//         while ((chkbit & x) == 0)
+//             x >>= 2;
+            
+//         // Return true iff the last bit is set.
+//         return ((x & chkbit) == 1);          
+//     }
+// }
 class Solution {
     public boolean isPowerOfFour(int x) {
-        // Binary form of 4 power is as follows
-        // 4 power 0 -> 1
-        // 4 power 1 -> 100
-        // 4 power 2 -> 10000     
-        if (x<1) return false;
-        if (x==1) return true;
-
-        int chkbit = 3;
-
-        // Check if the number has only one set bit
-        if ((x & (x - 1)) != 0)
+        return helper(x, 0);
+    }
+    private static boolean helper(int n, int pow){
+        if(n <= 0){
             return false;
-
-        // Left-shift the number by 2 bits and check
-        // if last two bits are zeros.
-        while ((chkbit & x) == 0)
-            x >>= 2;
-            
-        // Return true iff the last bit is set.
-        return ((x & chkbit) == 1);          
+        }
+        // Base condition
+        if(Math.pow(4, pow) >= n){
+            if(Math.pow(4, pow) == n){
+                return true;
+            } else {
+                return false;
+            }
+        }
+        
+        return helper(n, ++pow);
     }
 }
