1c6b52df0d45c36fde145eec62a255e1f5fff0fb
Ahmad jajja <ahmadjajja86@gmail.com>
2023-07-26 12:18:40 +0500

    Day-86 Recursion Maze problems

--- 18-recursion/code/backtracking/Maze.java
+++ 18-recursion/code/backtracking/Maze.java
@@ -1,93 +1,77 @@
-package com.kunal.backtracking;
-
 import java.util.ArrayList;
 
 public class Maze {
     public static void main(String[] args) {
-//        System.out.println(count(3, 3));
-//        path("", 3, 3);
-//        System.out.println(pathRet("", 3, 3));
-
-//        System.out.println(pathRetDiagonal("", 3, 3));
+        // System.out.println(count(3, 3) + " Paths");
+        // path("", 3, 3);
+        // System.out.println(pathList("", 3, 3));
+        // System.out.println(pathListDiagonal("", 3, 3));
 
         boolean[][] board = {
-                {true, true, true},
-                {true, false, true},
-                {true, true, true}
+            {true, true, true},
+            {true, false, true},
+            {true, true, true}
         };
-
         pathRestrictions("", board, 0, 0);
     }
-
-    static int count(int r, int c) {
+    private static int count(int r, int c){
         if (r == 1 || c == 1) {
             return 1;
         }
-        int left = count(r-1, c);
-        int right = count(r, c-1);
+        int left = count(r - 1, c);
+        int right = count(r, c - 1);
         return left + right;
     }
 
-    static void path(String p, int r, int c) {
+    private static void path(String p, int r, int c){
         if (r == 1 && c == 1) {
             System.out.println(p);
             return;
         }
-
         if (r > 1) {
-            path(p + 'D', r-1, c);
-        }
-
-        if (c > 1) {
-            path(p + 'R', r, c-1);
+            path(p + "D", r - 1, c);
+        } 
+        if (c > 1)  {
+            path(p + "R", r, c - 1);
         }
     }
-
-    static ArrayList<String> pathRet(String p, int r, int c) {
+    private static ArrayList<String> pathList(String p, int r, int c){
         if (r == 1 && c == 1) {
             ArrayList<String> list = new ArrayList<>();
             list.add(p);
             return list;
         }
-
+        
         ArrayList<String> list = new ArrayList<>();
-
         if (r > 1) {
-            list.addAll(pathRet(p + 'D', r-1, c));
+            list.addAll(pathList(p + "D", r - 1, c));
+        } 
+        if (c > 1)  {
+            list.addAll(pathList(p + "R", r, c - 1));
         }
-
-        if (c > 1) {
-            list.addAll(pathRet(p + 'R', r, c-1));
-        }
-
         return list;
     }
-
-    static ArrayList<String> pathRetDiagonal(String p, int r, int c) {
+    private static ArrayList<String> pathListDiagonal(String p, int r, int c){
         if (r == 1 && c == 1) {
             ArrayList<String> list = new ArrayList<>();
             list.add(p);
             return list;
         }
-
+        
         ArrayList<String> list = new ArrayList<>();
-
         if (r > 1 && c > 1) {
-            list.addAll(pathRetDiagonal(p + 'D', r-1, c-1));
-        }
-
+            list.addAll(pathListDiagonal(p + "H", r - 1, c - 1));
+        } 
         if (r > 1) {
-            list.addAll(pathRetDiagonal(p + 'V', r-1, c));
+            list.addAll(pathListDiagonal(p + "V", r - 1, c));
+        } 
+        if (c > 1)  {
+            list.addAll(pathListDiagonal(p + "H", r, c - 1));
         }
-
-        if (c > 1) {
-            list.addAll(pathRetDiagonal(p + 'H', r, c-1));
-        }
-
         return list;
     }
 
-    static void pathRestrictions(String p, boolean[][] maze, int r, int c) {
+    private static void pathRestrictions(String p,boolean[][] maze, int r, int c){
         if (r == maze.length - 1 && c == maze[0].length - 1) {
             System.out.println(p);
             return;
@@ -98,12 +82,10 @@ public class Maze {
         }
 
         if (r < maze.length - 1) {
-            pathRestrictions(p + 'D', maze, r+1, c);
-        }
-
-        if (c < maze[0].length - 1) {
-            pathRestrictions(p + 'R', maze, r, c+1);
+            pathRestrictions(p + "D",maze , r + 1, c);
+        } 
+        if (c < maze[0].length - 1)  {
+            pathRestrictions(p + "R",maze , r, c + 1);
         }
     }
-
 }
