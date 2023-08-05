b3f6cc3834673f258148d106a81048c631a629fc
Ahmad Jajja <86593662+Ahmadjajja@users.noreply.github.com>
2023-08-04 23:06:25 -0700

    Time: 487 ms (35.27%) | Memory: 40.2 MB (78.79%) - LeetSync

--- DailyLeetcoding/494-target-sum/target-sum.java
+++ DailyLeetcoding/494-target-sum/target-sum.java
@@ -1,56 +1,31 @@
-// class Solution {
-//     public int findTargetSumWays(int[] nums, int target) {
-//         if (nums.length == 1) {
-//             if (nums[0] == target || -nums[0] == target) {
-//                 return 1;
-//             } else {
-//                 return 0;
-//             }
-//         }
-//         return findExps(nums, target, 0, 0);
-//     }
-    
-//     private static int findExps(int[] nums, int target, int index, int sum) {
-//         // base condition
-//         if (index == nums.length) {
-//             if (sum == target) {
-//                 return 1;
-//             }
-//             return 0;
-//         }
-        
-//         int count = 0;
-//         // function call for + operator
-//         count += findExps(nums, target, index + 1, sum + nums[index]);
-        
-//         // function call for - operator
-//         count += findExps(nums, target, index + 1, sum - nums[index]);
-        
-//         return count;
-//     }
-// }
-
-
 class Solution {
-public int findTargetSumWays(int[] nums, int target) {
-
-    int ans=allsets(nums, 0 , 0, target, 0);
-    return ans;
-}
-
-public static int allsets(int[] nums,int idx, int sum, int target, int count){
-    
-    if(idx==nums.length){
-        if(sum==target){
-            count++;
+    public int findTargetSumWays(int[] nums, int target) {
+        if (nums.length == 1) {
+            if (nums[0] == target || -nums[0] == target) {
+                return 1;
+            } else {
+                return 0;
+            }
         }
-        return count;
+        return findExps(nums, target, 0, 0);
     }
     
-    int ansp=allsets(nums ,idx +1 , sum + nums[idx] ,target,count);
-    
-    int ansm=allsets(nums ,idx +1 , sum - nums[idx] ,target,count);
-
-    return ansp +ansm;
-}
+    private static int findExps(int[] nums, int target, int index, int sum) {
+        // base condition
+        if (index == nums.length) {
+            if (sum == target) {
+                return 1;
+            }
+            return 0;
+        }
+        
+        int count = 0;
+        // function call for + operator
+        count += findExps(nums, target, index + 1, sum + nums[index]);
+        
+        // function call for - operator
+        count += findExps(nums, target, index + 1, sum - nums[index]);
+        
+        return count;
+    }
 }
