c71fd90f936ec0b5da32e54f0c1c891532f3f583
Ahmad Jajja <86593662+Ahmadjajja@users.noreply.github.com>
2023-08-12 05:42:27 -0700

    Time: 2 ms (63.66%) | Memory: 39.5 MB (70.80%) - LeetSync

---
+++ DailyLeetcoding/1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.java
@@ -0,0 +1,42 @@
+class Solution {
+    public int findTheWinner(int n, int k) {
+        ArrayList<Integer> circle = new ArrayList<>();
+        for(int i = 1; i <= n; i++){
+            circle.add(i);
+        }
+        return winner(circle, 0, k);
+    }
+    private static int winner(ArrayList<Integer> circle, int s, int k){
+        if(circle.size() == 1){
+            return circle.get(0);
+        }
+        int removedPosition = (s + k - 1);
+        while(removedPosition >= circle.size()){
+            removedPosition -= circle.size();
+        }
+        circle.remove(removedPosition);
+        while(removedPosition >= circle.size()){
+            removedPosition -= circle.size();
+        }
+        return winner(circle, removedPosition, k);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
