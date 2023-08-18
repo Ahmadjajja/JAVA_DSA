f5bbcf489ff19d6590f598484f15f1b5783ca3f4
Ahmad Jajja <86593662+Ahmadjajja@users.noreply.github.com>
2023-08-18 10:23:45 -0700

    Time: 1 ms (35.59%) | Memory: 40.3 MB (32.91%) - LeetSync

--- DailyLeetcoding/216-combination-sum-iii/combination-sum-iii.java
+++ DailyLeetcoding/216-combination-sum-iii/combination-sum-iii.java
@@ -2,12 +2,12 @@ class Solution {
     public List<List<Integer>> combinationSum3(int k, int n) {
         List<List<Integer>> ans = new ArrayList<>();
         List<Integer> combinationSum = new ArrayList<>();
-        backtrack(ans, combinationSum, k, n, 1, 0);
+        backtrack(ans, combinationSum, k, n, 1);
         return ans;
     }
-    private static void backtrack(List<List<Integer>> ans, List<Integer> combinationSum, int k, int n, int s, int c){
-        if(n <= 0 || c == k){   // base condition
-            if(n == 0 && c == k){
+    private static void backtrack(List<List<Integer>> ans, List<Integer> combinationSum, int k, int n, int s){
+        if(n <= 0 || combinationSum.size() == k){   // base condition
+            if(n == 0 && combinationSum.size() == k){
                 ans.add(new ArrayList<>(combinationSum));
                 return;
             } else {
@@ -18,7 +18,7 @@ class Solution {
 
         for(int i = s; i <= 9; i++){
             combinationSum.add(i);
-            backtrack(ans, combinationSum, k, n - i, i + 1, c + 1);
+            backtrack(ans, combinationSum, k, n - i, i + 1);
             combinationSum.remove(combinationSum.size() - 1);
         }
     }
