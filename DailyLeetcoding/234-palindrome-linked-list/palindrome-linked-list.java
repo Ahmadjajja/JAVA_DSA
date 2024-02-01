8a590f2566ab423a413f2e9a18c0a50c44c12156
Ahmad Jajja <86593662+Ahmadjajja@users.noreply.github.com>
2024-02-01 14:44:50 +0500

    Time: 2212 ms (5.03%) | Memory: 79.4 MB (5.06%) - LeetSync

---
+++ DailyLeetcoding/234-palindrome-linked-list/palindrome-linked-list.java
@@ -0,0 +1,62 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        ListNode temp = head;
+        int size = 0, mid = 0;
+        while(temp != null){
+            temp = temp.next;
+            size++;
+        }
+        System.out.println("size : " + size);
+        if(size == 1) return true;
+        if(size % 2 == 0)
+            mid = size / 2;
+        else 
+            mid = size / 2 + 1;
+        return validPalindrome(head, size, mid);
+    }
+    public boolean validPalindrome(ListNode head, int size, int mid){
+        if( mid == 0 ){ // base condition
+            return true;
+        }
+        ListNode temp = head;
+        if(size == 2) {
+            System.out.println("temp.val : " + temp.val);
+            System.out.println("temp.next.val : " + temp.next.val);            
+            if(temp.val != temp.next.val){
+                return false;
+            }
+            return validPalindrome(head, 0, 0);
+        } 
+        if(size % 2 == 1){
+            System.out.println("Odd ");
+            System.out.println("size : " + size);
+            for(int i = 1; i < mid - 1; i++){
+                temp = temp.next;
+            }
+            temp.next = temp.next.next;
+            return validPalindrome(head, size - 1, (size - 1) / 2);
+        } else {
+            System.out.println("Even ");
+            System.out.println("size : " + size);
+            for(int i = 1; i < mid - 1; i++){
+                temp = temp.next;
+            }
+            if(temp.next.val != temp.next.next.val){
+                return false;
+            } else {
+                temp.next = temp.next.next.next;
+                return validPalindrome(head, size - 2, (size - 2) / 2);
+            }
+        }
+    }
+}
