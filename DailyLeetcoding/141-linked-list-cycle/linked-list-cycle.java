959231649bf2e2eea64931c10806029e308afd15
Ahmad Jajja <86593662+Ahmadjajja@users.noreply.github.com>
2024-02-02 22:26:04 +0500

    Time: 0 ms (100.00%) | Memory: 44.2 MB (65.40%) - LeetSync

--- DailyLeetcoding/141-linked-list-cycle/linked-list-cycle.java
+++ DailyLeetcoding/141-linked-list-cycle/linked-list-cycle.java
@@ -10,21 +10,41 @@
  * }
  */
 public class Solution {
+    // Time complexity -> O(n)
+    // Space complexity -> O(n)
+
+    // public boolean hasCycle(ListNode head) {
+    //     if(head == null || head.next == null){
+    //         return false;
+    //     }
+    //     LinkedList<ListNode> list = new LinkedList<>();
+    //     ListNode temp = head.next;
+    //     list.add(head);
+    //     while(temp != null){
+    //         if(list.contains(temp)){
+    //             return true;
+    //         }
+    //         list.add(temp);
+    //         temp = temp.next;
+    //     }
+    //     return false;
+    // }
+
+    // Time complexity -> O(n)
+    // Space complexity -> O(1)
+
     public boolean hasCycle(ListNode head) {
-        if(head == null || head.next == null){
-            return false;
-        }
-        LinkedList<ListNode> list = new LinkedList<>();
-        ListNode temp = head.next;
-        list.add(head);
-        while(temp != null){
-            if(list.contains(temp)){
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+            
+            // If match, cycle detected
+            if(slow == fast){
                 return true;
             }
-            list.add(temp);
-            temp = temp.next;
         }
         return false;
-
     }
 }
