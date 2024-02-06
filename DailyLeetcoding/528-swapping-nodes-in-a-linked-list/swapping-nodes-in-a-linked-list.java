f177395f45b03f8badb15da617959c4fcf4b5854
Ahmad Jajja <86593662+Ahmadjajja@users.noreply.github.com>
2024-02-06 16:44:45 +0500

    Time: 1 ms (100.00%) | Memory: 58.1 MB (66.96%) - LeetSync

--- DailyLeetcoding/528-swapping-nodes-in-a-linked-list/swapping-nodes-in-a-linked-list.java
+++ DailyLeetcoding/528-swapping-nodes-in-a-linked-list/swapping-nodes-in-a-linked-list.java
@@ -8,60 +8,98 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+
+
+// //  Swapping nodes
+// // time complexity -> O(n)
+// // space complexity -> O(1)
+
+// class Solution {
+//     public ListNode swapNodes(ListNode head, int k) {
+//         ListNode dumyNode = new ListNode(0, head); // used here for edge case
+//         ListNode leftPrev = null;
+//         ListNode rightPrev = null;
+//         // finding size of a list
+//         ListNode temp = head;
+//         int size = 0;
+//         while(temp != null){
+//             temp = temp.next;
+//             size = size + 1;
+//         }
+//         temp = head;
+//         for(int i = 1; i <= size; i++){
+//             if(i == k - 1){
+//                 leftPrev = temp;
+//             }
+//             if(i == size - k){
+//                 rightPrev = temp;
+//             }
+//             temp = temp.next;
+//         }
+//         // for edge case
+//         if(leftPrev == null){
+//             leftPrev = dumyNode;
+//         }
+//         if(rightPrev == null){
+//             rightPrev = dumyNode;
+//         }
+//         ListNode left = leftPrev.next;
+//         ListNode right = rightPrev.next;
+//         if(left == right){
+//             return head;
+//         }
+//         // if left and right nodes are adjacent
+//         if(left.next == right || right.next == left){ 
+//             if(left.next == right){
+//                 // if left came before than right
+//                 leftPrev.next = right;
+//                 left.next = right.next;
+//                 right.next = left;
+//             } else {
+//                 // if right came before than left
+//                 rightPrev.next = left;
+//                 right.next = left.next;
+//                 left.next = right;
+//             }
+//         } else {
+//             ListNode tempNext = left.next;
+//             left.next = right.next;
+//             right.next = tempNext;
+//             rightPrev.next = left;
+//             leftPrev.next = right; 
+//         }
+//         return dumyNode.next;
+//     }
+// }
+
+//  Swapping node's values
+// time complexity -> O(n)
+// space complexity -> O(1)
 class Solution {
     public ListNode swapNodes(ListNode head, int k) {
-        ListNode dumyNode = new ListNode(0, head); // used here for edge case
-        ListNode leftPrev = null;
-        ListNode rightPrev = null;
-        // finding size of a list
-        ListNode temp = head;
-        int size = 0;
-        while(temp != null){
-            temp = temp.next;
-            size = size + 1;
+        ListNode left = head;
+        ListNode right = head;
+        ListNode cur = head;
+
+        for(int i = 1; i < k && cur != null; i++){
+            cur = cur.next;
         }
-        temp = head;
-        for(int i = 1; i <= size; i++){
-            if(i == k - 1){
-                leftPrev = temp;
-            }
-            if(i == size - k){
-                rightPrev = temp;
-            }
-            temp = temp.next;
-        }
-        // for edge case
-        if(leftPrev == null){
-            leftPrev = dumyNode;
-        }
-        if(rightPrev == null){
-            rightPrev = dumyNode;
-        }
-        ListNode left = leftPrev.next;
-        ListNode right = rightPrev.next;
-        if(left == right){
+
+        // If 'cur' is null, k is out of bounds, return head as no swapping can be done
+        if (cur == null) {
             return head;
         }
-        // if left and right nodes are adjacent
-        if(left.next == right || right.next == left){ 
-            if(left.next == right){
-                // if left came before than right
-                leftPrev.next = right;
-                left.next = right.next;
-                right.next = left;
-            } else {
-                // if right came before than left
-                rightPrev.next = left;
-                right.next = left.next;
-                left.next = right;
-            }
-        } else {
-            ListNode tempNext = left.next;
-            left.next = right.next;
-            right.next = tempNext;
-            rightPrev.next = left;
-            leftPrev.next = right; 
+
+        left = cur;
+        cur = cur.next;
+
+        while(cur != null){
+            cur = cur.next;
+            right = right.next;
         }
-        return dumyNode.next;
+        int temp = left.val;
+        left.val = right.val;
+        right.val = temp;
+        return head;
     }
 }
