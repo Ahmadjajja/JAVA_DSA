b88dc40ccd0dafa4591db610a6241ecd68aecb22
Ahmad Jajja <86593662+Ahmadjajja@users.noreply.github.com>
2024-02-16 12:21:39 +0500

    Time: 36 ms (81.76%) | Memory: 45.5 MB (77.53%) - LeetSync

--- DailyLeetcoding/2470-removing-stars-from-a-string/removing-stars-from-a-string.java
+++ DailyLeetcoding/2470-removing-stars-from-a-string/removing-stars-from-a-string.java
@@ -1,24 +1,44 @@
+// // Stack solution
+
+// class Solution {
+//     public String removeStars(String s) {
+//         Stack<Character> stack = new Stack<>();
+
+//         for(int i = 0; i < s.length(); i++){
+//             if(s.charAt(i) == '*'){
+//                 if (!stack.isEmpty()) {
+//                     stack.pop();
+//                 }
+//             } else {
+//                 stack.push(s.charAt(i));
+//             }
+//         }
+
+//         StringBuilder ans = new StringBuilder();
+
+//         while(!stack.empty()){
+//             ans.insert(0, stack.pop());
+//         }
+
+//         return ans.toString();
+//     }
+// }
+
+
+// Simple string builder solution
 
 class Solution {
-    public String removeStars(String s) {
-        Stack<Character> stack = new Stack<>();
+    public String removeStars(final String s) {
+        final StringBuilder sb = new StringBuilder();
 
-        for(int i = 0; i < s.length(); i++){
-            if(s.charAt(i) == '*'){
-                if (!stack.isEmpty()) {
-                    stack.pop();
-                }
-            } else {
-                stack.push(s.charAt(i));
+        for(int i = 0; i < s.length(); ++i) {
+            if(sb.length() > 0 && s.charAt(i) == '*') {
+                sb.setLength(sb.length() - 1);
+            } else if(s.charAt(i) != '*') {
+                sb.append(s.charAt(i));
             }
         }
 
-        StringBuilder ans = new StringBuilder();
-
-        while(!stack.empty()){
-            ans.insert(0, stack.pop());
-        }
-
-        return ans.toString();
+        return sb.toString();
     }
 }
