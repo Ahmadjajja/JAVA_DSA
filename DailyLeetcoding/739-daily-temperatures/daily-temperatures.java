a4cabf11dc76069cdfb01f8dd42c0c089caeea4d
Ahmad Jajja <86593662+Ahmadjajja@users.noreply.github.com>
2024-02-19 17:12:02 +0500

    Time: 64 ms (54.84%) | Memory: 62.8 MB (23.32%) - LeetSync

--- DailyLeetcoding/739-daily-temperatures/daily-temperatures.java
+++ DailyLeetcoding/739-daily-temperatures/daily-temperatures.java
@@ -6,14 +6,13 @@ class Solution {
 
         for(int i = 0; i < temperatures.length; i++){
 
-            output[i] = 0;
-
             stack.push(i);
 
             while(!stack.empty() && i + 1 < temperatures.length && temperatures[i + 1] > temperatures[stack.peek()]){
+                
                 int index = stack.pop();
-
                 output[index] = i + 1 - index;
+                
             }
 
         }
