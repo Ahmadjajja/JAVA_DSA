dab9c059ab38ef41aa2c2116c4f7876676f4f604
Ahmad Jajja <86593662+Ahmadjajja@users.noreply.github.com>
2024-02-25 21:41:28 +0500

    Time: 8 ms (48.36%) | Memory: 40.1 MB (81.73%) - LeetSync

--- DailyLeetcoding/1013-fibonacci-number/fibonacci-number.java
+++ DailyLeetcoding/1013-fibonacci-number/fibonacci-number.java
@@ -1,10 +1,9 @@
 class Solution {
     public int fib(int n) {
-        if(n == 0 || n == 1){
-            return n;
+        if(n <= 1){
+            return n ;
         }
-        int fib1 = fib(n - 1);
-        int fib2 = fib(n - 2);
-        return fib1 + fib2;
-    }
+        return fib(n - 1) + fib(n - 2);
+    } 
 }
+
