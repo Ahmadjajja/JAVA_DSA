142b1a611eb4faf6a2c0e49f7030122e745460c7
Ahmad Jajja <86593662+Ahmadjajja@users.noreply.github.com>
2024-02-28 03:34:15 +0500

    Time: 34 ms (75.46%) | Memory: 16.5 MB (93.95%) - LeetSync

---
+++ DailyLeetcoding/1127-last-stone-weight/last-stone-weight.py
@@ -0,0 +1,19 @@
+from typing import List
+import heapq
+
+class Solution:
+    def lastStoneWeight(self, stones: List[int]) -> int:
+        # Negate all values to create a max heap
+        maxHeap = [-stone for stone in stones]
+        heapq.heapify(maxHeap)  # Convert the list to a heap in-place
+
+        # While there are more than 1 stone remaining
+        while len(maxHeap) > 1:
+            val1 = -heapq.heappop(maxHeap)  # Pop the largest stone
+            val2 = -heapq.heappop(maxHeap)  # Pop the second largest stone
+
+            if val1 != val2:
+                remainElem = val1 - val2
+                heapq.heappush(maxHeap, -remainElem)  # Push the remaining stone back into maxHeap
+
+        return -maxHeap[0] if len(maxHeap) == 1 else 0
