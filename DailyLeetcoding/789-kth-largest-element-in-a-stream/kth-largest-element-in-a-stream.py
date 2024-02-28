9538fe13ea956752c1cb58bdb057744cee48616d
Ahmad Jajja <86593662+Ahmadjajja@users.noreply.github.com>
2024-02-28 22:43:58 +0500

    Time: 89 ms (25.13%) | Memory: 20.5 MB (49.72%) - LeetSync

--- DailyLeetcoding/789-kth-largest-element-in-a-stream/kth-largest-element-in-a-stream.py
+++ DailyLeetcoding/789-kth-largest-element-in-a-stream/kth-largest-element-in-a-stream.py
@@ -1,18 +1,26 @@
 class KthLargest:
 
     def __init__(self, k: int, nums: List[int]):
-        # minHeap with K largest integers
-        # creating member funtions of class
         self.minHeap, self.k = nums, k
-        # converting array( minHeap ) to Min Heap that will create the sorted property
-        heapq.heapify(self.minHeap)
-        # It will run in O(n) times, but it could be have more than K elements
+
+        heapq.heapify(self.minHeap);
+
+
+    def add(self, val: int) -> int:
+        heapq.heappush(self.minHeap, val);
+
         while len(self.minHeap) > self.k:
             heapq.heappop(self.minHeap)
 
-    def add(self, val: int) -> int:
-        heapq.heappush(self.minHeap, val)
-        if len(self.minHeap) > self.k:
-            heapq.heappop(self.minHeap)
-        return self.minHeap[0]
+        return self.minHeap[0];
+
+
+
+# There are two things on leetcode
+# 1 - understand the problem statement, then try to think & write on paper 
+#     how the answers looks like by comparing with the question
+
+# 2 - if you know all data structure & the answer will be clicked in
+#     your mind at the same time
+
 
