15ceecf60c87ecd9077814850c45f098e55058a2
Ahmad Jajja <86593662+Ahmadjajja@users.noreply.github.com>
2024-03-04 23:01:01 +0500

    Time: 30 ms (92.80%) | Memory: 16.7 MB (84.12%) - LeetSync

--- DailyLeetcoding/78-subsets/subsets.py
+++ DailyLeetcoding/78-subsets/subsets.py
@@ -2,20 +2,22 @@ class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
         res = []
         subset = []
-        
-        def dfs(i):
-            if i >= len(nums):
-                res.append(subset.copy())
-                return
-            
-            # decision to include nums[i]
-            subset.append(nums[i])
-            dfs(i + 1)
 
-            # decision NOT to include nums[i]
+        def dfs(index):
+            print("subset ->" , subset)
+            if index == len(nums):
+                res.append(subset.copy())
+                return 
+
+            # include index value
+            subset.append(nums[index])
+            dfs(index + 1)
+
+            # exclude index value
             subset.pop()
-            dfs(i + 1)
-            
+            dfs(index + 1)
+
+
         dfs(0)
 
         return res
