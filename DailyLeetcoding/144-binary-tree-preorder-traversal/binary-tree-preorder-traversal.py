2329d0fe494df0202309212e633d1b3edfae622c
Ahmad Jajja <86593662+Ahmadjajja@users.noreply.github.com>
2024-03-12 07:01:49 +0500

    Time: 35 ms (55.81%) | Memory: 16.7 MB (16.49%) - LeetSync

--- DailyLeetcoding/144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.py
+++ DailyLeetcoding/144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.py
@@ -5,16 +5,50 @@
 #         self.left = left
 #         self.right = right
 
+
+# # Recursive solution
+# class Solution:
+#     def preorderTraversal(self, root: TreeNode) -> List[int]:
+#         res = []
+
+#         def POT(sRoot: TreeNode):
+
+#             # base condition
+#             if not sRoot:
+#                 return
+            
+#             res.append(sRoot.val)
+#             POT(sRoot.left)
+#             POT(sRoot.right)
+#         POT(root)
+
+#         return res
+
 # Recursive solution
 class Solution:
     def preorderTraversal(self, root: TreeNode) -> List[int]:
+        cur, stack = root, []
         res = []
-        if not root:
-            return res
 
-        res.append(root.val)
-        res += self.preorderTraversal(root.left)
-        res += self.preorderTraversal(root.right)
-
+        while cur or stack:
+            if cur:
+                res.append(cur.val)
+                stack.append(cur.right)
+                cur = cur.left
+            else:
+                cur = stack.pop()
+        
         return res
+
+
+
+
+
+
+
+
+
+
+
+
 
