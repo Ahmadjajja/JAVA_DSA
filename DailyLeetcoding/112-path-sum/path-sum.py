1b3e929bb569f0fb11c22d09c3e35d640a606cb0
Ahmad Jajja <86593662+Ahmadjajja@users.noreply.github.com>
2024-03-13 06:46:34 +0500

    Time: 44 ms (36.16%) | Memory: 17.3 MB (99.20%) - LeetSync

--- DailyLeetcoding/112-path-sum/path-sum.py
+++ DailyLeetcoding/112-path-sum/path-sum.py
@@ -8,22 +8,19 @@ class Solution:
     def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
         if not root:
             return False
-
-        def traverseTree(cur: Optional[TreeNode], sum) -> bool:
-
-            # base case
+        
+        def traverse(cur: Optional[TreeNode], sum) -> bool:
+            # base condition
             if not cur:
                 return False
-            
             
             sum += cur.val
 
             if not cur.left and not cur.right:
                 return sum == targetSum
-
-            return traverseTree(cur.left, sum) or traverseTree(cur.right, sum)
+            
+            return traverse(cur.left, sum) or traverse(cur.right, sum)
         
-        
-        return traverseTree(root, 0)
+        return traverse(root, 0)
             
 
