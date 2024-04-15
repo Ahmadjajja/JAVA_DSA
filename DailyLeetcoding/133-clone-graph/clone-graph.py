588e9d67ed24418a41a35e5520bf93bb10e333bd
Ahmad Jajja <86593662+Ahmadjajja@users.noreply.github.com>
2024-04-15 07:10:43 +0500

    Time: 40 ms (59.30%) | Memory: 16.9 MB (85.61%) - LeetSync

--- DailyLeetcoding/133-clone-graph/clone-graph.py
+++ DailyLeetcoding/133-clone-graph/clone-graph.py
@@ -1,18 +1,31 @@
-"""
-# Definition for a Node.
-class Node:
-    def __init__(self, val = 0, neighbors = None):
-        self.val = val
-        self.neighbors = neighbors if neighbors is not None else []
-"""
+# """
+# # Definition for a Node.
+# class Node:
+#     def __init__(self, val = 0, neighbors = None):
+#         self.val = val
+#         self.neighbors = neighbors if neighbors is not None else []
+# """
 
-from typing import Optional
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 class Solution:
     def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
         oldToNew = {}
 
         def dfs(node):
-
+            # breaking-point or base-case
             if node in oldToNew:
                 return oldToNew[node]
             
@@ -21,6 +34,27 @@ class Solution:
 
             for nei in node.neighbors:
                 copy.neighbors.append(dfs(nei))
+            
             return copy
-        
+
+
+
+        # Edge-Case
         return dfs(node) if node else None
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
