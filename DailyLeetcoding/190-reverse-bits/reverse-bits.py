23f16f85b46ac1045c519de46ef3f15f3f3e1251
Ahmad Jajja <86593662+Ahmadjajja@users.noreply.github.com>
2024-07-30 18:41:03 +0500

    Time: 28 ms (93.94%) | Memory: 16.6 MB (32.65%) - LeetSync

--- DailyLeetcoding/190-reverse-bits/reverse-bits.py
+++ DailyLeetcoding/190-reverse-bits/reverse-bits.py
@@ -25,7 +25,9 @@ class Solution:
         res = 0
 
         for i in  range(32):
+
             bit = (n >> i) & 1
-            res = res | (bit << (31 - i))
+            
+            res = res + (bit << (31 - i))
 
         return res
