78cec7fd7601881621f9e5299b09f3eb34de127c
Ahmad Jajja <86593662+Ahmadjajja@users.noreply.github.com>
2024-07-30 14:00:37 +0500

    Time: 30 ms (89.86%) | Memory: 16.4 MB (77.92%) - LeetSync

--- DailyLeetcoding/190-reverse-bits/reverse-bits.py
+++ DailyLeetcoding/190-reverse-bits/reverse-bits.py
@@ -1,18 +1,31 @@
+# # Normal Solution
+# class Solution:
+#     def reverseBits(self, n: int) -> int:
+#         count = 0
+#         listArr = []
+#         while n:
+#             listArr.append(n % 2)
+#             n = n >> 1
+#             count += 1
+
+#         for i in range(32 - count):
+#             listArr.append(0)
+
+#         print("listArr length:", len(listArr))
+#         print("listArr:", listArr)
+
+#         # Convert listArr to a string of bits, then parse as an integer
+#         reversed_num = int("".join(map(str, listArr)), 2)
+#         return reversed_num
+
+# Optimized solution
+
 class Solution:
     def reverseBits(self, n: int) -> int:
-        count = 0
-        listArr = []
-        while n:
-            listArr.append(n % 2)
-            n = n >> 1
-            count += 1
+        res = 0
 
-        for i in range(32 - count):
-            listArr.append(0)
+        for i in  range(32):
+            bit = (n >> i) & 1
+            res = res | (bit << (31 - i))
 
-        print("listArr length:", len(listArr))
-        print("listArr:", listArr)
-
-        # Convert listArr to a string of bits, then parse as an integer
-        reversed_num = int("".join(map(str, listArr)), 2)
-        return reversed_num
+        return res
