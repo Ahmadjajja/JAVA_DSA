144cfa8653e4abf7fb292775d505793f28a5f293
Ahmad Jajja <86593662+Ahmadjajja@users.noreply.github.com>
2024-08-12 10:49:11 +0500

    Time: 48 ms (62.79%) | Memory: 16.9 MB (84.50%) - LeetSync

--- DailyLeetcoding/242-valid-anagram/valid-anagram.py
+++ DailyLeetcoding/242-valid-anagram/valid-anagram.py
@@ -1,5 +1,7 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
 
         def countFrequency(word):
             f = {} # hashmap
