a08ef80012825d449c64a63ff830af18869b1598
Ahmad Jajja <86593662+Ahmadjajja@users.noreply.github.com>
2024-08-12 10:43:47 +0500

    Time: 49 ms (57.70%) | Memory: 16.9 MB (84.50%) - LeetSync

---
+++ DailyLeetcoding/242-valid-anagram/valid-anagram.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        
+        def countFrequency(word):
+            f = {} # hashmap
+            for ch in word:
+                if ch in f:
+                    f[ch] += 1
+                else: 
+                    f[ch] = 1
+            return f
+
+        return countFrequency(s) == countFrequency(t)
+        
