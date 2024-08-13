c4ffa2396457b96b157cd41c0b89a203db738eb8
Ahmad Jajja <86593662+Ahmadjajja@users.noreply.github.com>
2024-08-13 12:18:50 +0500

    Time: 59 ms (65.40%) | Memory: 17.7 MB (52.43%) - LeetSync

--- DailyLeetcoding/1-two-sum/two-sum.py
+++ DailyLeetcoding/1-two-sum/two-sum.py
@@ -1,7 +1,18 @@
 class Solution:
+    # # brute-force solution
+    # def twoSum(self, nums: List[int], target: int) -> List[int]:
+    #     for i in range(len(nums)):
+    #         for j in range(i+1, len(nums)):
+    #             if nums[i] + nums[j] == target:
+    #                 return [i, j]
+    #     return [-1, -1]
+    
+    # efficient-solution
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        for i in range(len(nums)):
-            for j in range(i+1, len(nums)):
-                if nums[i] + nums[j] == target:
-                    return [i, j]
-        return [-1, -1]
+        prevMap = {} # val : index
+
+        for i, n in enumerate(nums):
+            diff = target - n
+            if diff in prevMap:
+                return [prevMap[diff], i]
+            prevMap[n] = i
