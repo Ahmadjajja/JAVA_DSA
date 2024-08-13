f6cec8fc78a40f1c221cfb5c0e342a4bfe19789e
Ahmad Jajja <86593662+Ahmadjajja@users.noreply.github.com>
2024-08-13 18:21:43 +0500

    Time: 86 ms (74.44%) | Memory: 22.4 MB (9.31%) - LeetSync

--- DailyLeetcoding/49-group-anagrams/group-anagrams.py
+++ DailyLeetcoding/49-group-anagrams/group-anagrams.py
@@ -1,24 +1,13 @@
-# class Solution:
-#     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-#         res = defaultdict(list) # mapping charCount to list of anagrams
-
-#         for s in strs:
-#             count = [0] * 26 # a ... z
-
-#             for c in s:
-#                 count[ord(c) - ord("a")] =+ 1
-            
-#             res[tuple(count)].append(s) # list can't be key, that's why converted into tuple
-        
-#         return res.values()
-
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        ans = collections.defaultdict(list)
+        ans = defaultdict(list) # mapping charCount to list of anagrams
 
         for s in strs:
-            count = [0] * 26
+            count = [0] * 26 # a ... z
+
             for c in s:
                 count[ord(c) - ord("a")] += 1
-            ans[tuple(count)].append(s)
+            
+            ans[tuple(count)].append(s) # list can't be key, that's why converted into tuple
+        
         return ans.values()
