647a4900f52e2120692ae1146ae279d5e75b7099
Ahmad Jajja <86593662+Ahmadjajja@users.noreply.github.com>
2024-08-14 21:58:05 +0500

    Time: 42 ms (73.25%) | Memory: 17 MB (55.11%) - LeetSync

---
+++ DailyLeetcoding/125-valid-palindrome/valid-palindrome.py
@@ -0,0 +1,31 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        new = ''
+        for ch in s:
+            if ch.isalpha() or ch.isdigit():
+                new += ch.lower()
+        
+        return new == new[::-1]
+            
+
+
+    # def isPalindrome(self, s: str) -> bool:
+
+    #     l, r = 0, len(s) - 1
+
+    #     while l < r:
+    #         while l < r and not self.isAlphanumeric(s[l]):
+    #             l += 1
+    #         while r > l and not self.isAlphanumeric(s[r]):
+    #             r -= 1
+            
+    #         if s[l].lower() != s[r].lower():
+    #             return False
+    #         l, r = l + 1, r - 1
+    #     return True
+
+    # def isAlphanumeric(self, c):
+    #     return (ord('A') <= ord(c) <= ord('Z') or ord('a') <= ord(c) <= ord('z') or ord('0') <= ord(c) <= ord('9'))
+        
+        
+
