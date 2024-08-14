79e82cf821c6411c6c16ec18d926c17efed7bf88
Ahmad Jajja <86593662+Ahmadjajja@users.noreply.github.com>
2024-08-14 21:59:00 +0500

    Time: 58 ms (11.46%) | Memory: 17 MB (55.11%) - LeetSync

--- DailyLeetcoding/125-valid-palindrome/valid-palindrome.py
+++ DailyLeetcoding/125-valid-palindrome/valid-palindrome.py
@@ -1,32 +1,33 @@
-class Solution:
-    def isPalindrome(self, s: str) -> bool:
-        new = ''
-        for ch in s:
-            if ch.isalpha() or ch.isdigit():
-                new += ch.lower()
+# class Solution:
+#     def isPalindrome(self, s: str) -> bool:
+#         new = ''
+#         for ch in s:
+#             if ch.isalpha() or ch.isdigit():
+#                 new += ch.lower()
         
-        return new == new[::-1]
+#         return new == new[::-1]
             
 
 # Efficient Solution
-# class Solution:
-    # def isPalindrome(self, s: str) -> bool:
 
-    #     l, r = 0, len(s) - 1
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
 
-    #     while l < r:
-    #         while l < r and not self.isAlphanumeric(s[l]):
-    #             l += 1
-    #         while r > l and not self.isAlphanumeric(s[r]):
-    #             r -= 1
+        l, r = 0, len(s) - 1
+
+        while l < r:
+            while l < r and not self.isAlphanumeric(s[l]):
+                l += 1
+            while r > l and not self.isAlphanumeric(s[r]):
+                r -= 1
             
-    #         if s[l].lower() != s[r].lower():
-    #             return False
-    #         l, r = l + 1, r - 1
-    #     return True
+            if s[l].lower() != s[r].lower():
+                return False
+            l, r = l + 1, r - 1
+        return True
 
-    # def isAlphanumeric(self, c):
-    #     return (ord('A') <= ord(c) <= ord('Z') or ord('a') <= ord(c) <= ord('z') or ord('0') <= ord(c) <= ord('9'))
+    def isAlphanumeric(self, c):
+        return (ord('A') <= ord(c) <= ord('Z') or ord('a') <= ord(c) <= ord('z') or ord('0') <= ord(c) <= ord('9'))
         
         
 
