946d51af3bda723a0769d69321a8f2ec42dad110
Ahmad Jajja <86593662+Ahmadjajja@users.noreply.github.com>
2024-08-17 12:03:22 +0500

    Time: 101 ms (81.55%) | Memory: 17.3 MB (70.44%) - LeetSync

--- DailyLeetcoding/76-minimum-window-substring/minimum-window-substring.py
+++ DailyLeetcoding/76-minimum-window-substring/minimum-window-substring.py
@@ -1,42 +1,98 @@
+# class Solution:
+#     def minWindow(self, s: str, t: str) -> str:
+#         if t == "":
+#             return ""
+
+#         countT, window = {}, {}
+
+#         for c in t:
+#             countT[c] = 1 + countT.get(c, 0)
+        
+#         have, need = 0, len(countT)
+
+#         res, resLength = [-1, -1], float('infinity')
+#         l = 0
+
+#         for r in range(len(s)):
+#             c = s[r]
+#             window[c] = 1 + window.get(c, 0)
+
+#             if c in countT and window[c] == countT[c]:
+#                 have += 1
+            
+#             while have == need:
+#                 # update our result
+#                 if (r - l + 1) < resLength:
+#                     res = [l, r]
+#                     resLength = r - l + 1
+                
+#                 # pop from the left of our window
+#                 window[s[l]] -= 1
+
+#                 if s[l] in countT and window[s[l]] < countT[s[l]]:
+#                     have -= 1
+                
+#                 l += 1
+            
+#         l, r = res
+
+#         return s[l: r + 1] if resLength != float("infinity") else ""
+
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
         if t == "":
             return ""
-
-        countT, window = {}, {}
+        
+        countC, windowC = {}, {}
 
         for c in t:
-            countT[c] = 1 + countT.get(c, 0)
+            countC[c] = 1 + countC.get(c, 0)
         
-        have, need = 0, len(countT)
-
-        res, resLength = [-1, -1], float('infinity')
+        have, need = 0, len(countC)
+        res, resLen = [-1, -1], float('infinity')
         l = 0
 
         for r in range(len(s)):
-            c = s[r]
-            window[c] = 1 + window.get(c, 0)
+            ch = s[r]
+            windowC[ch] = 1 + windowC.get(ch, 0)
 
-            if c in countT and window[c] == countT[c]:
-                have += 1
-            
+            if ch in countC and windowC[ch] == countC[ch]: 
+                    have += 1
+
             while have == need:
                 # update our result
-                if (r - l + 1) < resLength:
+                if (r - l + 1) < resLen:
                     res = [l, r]
-                    resLength = r - l + 1
-                
-                # pop from the left of our window
-                window[s[l]] -= 1
+                    resLen = r - l + 1
 
-                if s[l] in countT and window[s[l]] < countT[s[l]]:
+
+                windowC[s[l]] -= 1
+
+                if s[l] in countC and windowC[s[l]] < countC[s[l]]:
                     have -= 1
-                
+
                 l += 1
-            
+
         l, r = res
 
-        return s[l: r + 1] if resLength != float("infinity") else ""
+        return s[l : r + 1] if resLen != float("infinity") else ""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
