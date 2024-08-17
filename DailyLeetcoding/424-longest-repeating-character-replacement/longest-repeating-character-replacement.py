4075d2bdd96463e86846facba219eaf1dc27a92a
Ahmad Jajja <86593662+Ahmadjajja@users.noreply.github.com>
2024-08-17 14:30:27 +0500

    Time: 120 ms (29.82%) | Memory: 16.7 MB (34.93%) - LeetSync

--- DailyLeetcoding/424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
+++ DailyLeetcoding/424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
@@ -1,45 +1,24 @@
-# class Solution:
-#     def characterReplacement(self, s: str, k: int) -> int:
-#         count = {}
-#         res = 0
-
-#         l = 0
-#         maxf = 0
-#         for r in range(len(s)):
-#             count[s[r]] = 1 + count.get(s[r], 0)
-#             # maxf = max(maxf, count[s[r]])
-
-#             while (r - l + 1) - max(count.values()) > k:
-#                 count[s[l]] -= 1
-#                 l += 1
-            
-#             res = max(res, r - l + 1)
-        
-#         return res
-
-
-
-
-
-
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        ans = 0
-        countF = {}
+        count = {}
+        res = 0
         l = 0
-        maxC = 0
+        maxf = 0
+        
         for r in range(len(s)):
-            ch = s[r]
-            countF[ch] = 1 + countF.get(ch, 0)
+            count[s[r]] = 1 + count.get(s[r], 0)
 
-            
-            while (r - l + 1) - max(countF.values()) > k:
-                countF[s[l]] -= 1
+            while (r - l + 1) - max(count.values()) > k:
+                count[s[l]] -= 1
                 l += 1
             
-            ans = max(ans, r - l + 1)
-            
-        return ans
+            res = max(res, r - l + 1)
+        
+        return res
+
+
+
+
 
 
 
