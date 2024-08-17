a2d7e51e934cd6e87a278ab53b3267138e75ac26
Ahmad Jajja <86593662+Ahmadjajja@users.noreply.github.com>
2024-08-17 14:27:30 +0500

    Time: 113 ms (38.32%) | Memory: 16.6 MB (71.07%) - LeetSync

--- DailyLeetcoding/424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
+++ DailyLeetcoding/424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
@@ -1,19 +1,63 @@
+# class Solution:
+#     def characterReplacement(self, s: str, k: int) -> int:
+#         count = {}
+#         res = 0
+
+#         l = 0
+#         maxf = 0
+#         for r in range(len(s)):
+#             count[s[r]] = 1 + count.get(s[r], 0)
+#             # maxf = max(maxf, count[s[r]])
+
+#             while (r - l + 1) - max(count.values()) > k:
+#                 count[s[l]] -= 1
+#                 l += 1
+            
+#             res = max(res, r - l + 1)
+        
+#         return res
+
+
+
+
+
+
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        count = {}
-        res = 0
-
+        ans = 0
+        countF = {}
         l = 0
-        maxf = 0
+        maxC = 0
         for r in range(len(s)):
-            count[s[r]] = 1 + count.get(s[r], 0)
-            # maxf = max(maxf, count[s[r]])
+            ch = s[r]
+            countF[ch] = 1 + countF.get(ch, 0)
 
-            while (r - l + 1) - max(count.values()) > k:
-                count[s[l]] -= 1
-                l += 1
+            maxC = max(countF.values())
+
             
-            res = max(res, r - l + 1)
-        
-        return res
+            while (r - l + 1) - maxC > k:
+                countF[s[l]] -= 1
+                l += 1
+                maxC = max(countF.values())
+            
+            ans = max(ans, r - l + 1)
+            
+        return ans
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
