d964b82f3d00ba525eadaa5fcf8f9a4e14c6f10a
Ahmad Jajja <86593662+Ahmadjajja@users.noreply.github.com>
2024-09-02 17:28:10 +0500

    Time: 20 ms (99.96%) | Memory: 17.1 MB (70.46%) - LeetSync

--- DailyLeetcoding/102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.py
+++ DailyLeetcoding/102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.py
@@ -4,23 +4,49 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
+# depth-first solution
+
+# class Solution:
+#     def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+#         hashmap = defaultdict(list)
+
+#         def dfs(node, level):
+
+#             if not node:
+#                 return
+            
+#             hashmap[level].append(node.val)
+
+#             dfs(node.left, level + 1)
+#             dfs(node.right, level + 1)
+
+#         dfs(root, 0)
+
+#         return hashmap.values()
+
+
+# breadth-first solution
 class Solution:
     def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
-        hashmap = defaultdict(list)
+        res = []
+        q = collections.deque()
+        q.append(root)
 
-        def dfs(node, level):
+        while q:
+            qLen = len(q)
+            level = []
 
-            if not node:
-                return
-            
-            hashmap[level].append(node.val)
-
-            dfs(node.left, level + 1)
-            dfs(node.right, level + 1)
-
-        dfs(root, 0)
-
-        return hashmap.values()
+            for i in range(qLen):
+                node = q.popleft()
+                if node:
+                    level.append(node.val)
+                    q.append(node.left)
+                    q.append(node.right)
+            if level:
+                res.append(level)
+                
+        return res
 
             
 
