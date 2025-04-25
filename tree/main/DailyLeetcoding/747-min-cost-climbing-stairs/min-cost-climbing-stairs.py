55129d816d43b7f521780f1048524366d36d6e1b
Ahmad Jajja <86593662+Ahmadjajja@users.noreply.github.com>
2025-04-24 21:07:10 -0600

    Time: 3 ms (73.29%) | Memory: 18 MB (29.51%) - LeetSync

--- tree/main/DailyLeetcoding/747-min-cost-climbing-stairs/min-cost-climbing-stairs.py
+++ tree/main/DailyLeetcoding/747-min-cost-climbing-stairs/min-cost-climbing-stairs.py
@@ -1,18 +1,29 @@
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
-        # let's create a cache to store repeated recursion calls
-        cache = [-1] * (len(cost) + 1)
+        # dynamic programming solution
+        cost.append(0)
 
-        def helper(index):
+        for i in range(len(cost) - 3, -1, -1):
+            cost[i] += min(cost[i + 1], cost[i + 2])
+        
+        return min(cost[0], cost[1])
 
-            if index >= len(cost):
-                return 0
+        # # let's create a cache to store repeated recursion calls
+        # cache = [-1] * (len(cost) + 1)
+
+        # def helper(index):
+
+        #     if index >= len(cost):
+        #         return 0
             
-            if cache[index] != -1:
-                return cache[index]
+        #     if cache[index] != -1:
+        #         return cache[index]
 
-            cache[index] = cost[index] + min(helper(index + 1), helper(index + 2))
+        #     cache[index] = cost[index] + min(helper(index + 1), helper(index + 2))
 
-            return cache[index]
+        #     return cache[index]
 
-        return min(helper(0), helper(1))
+        # return min(helper(0), helper(1))
+
+
+
