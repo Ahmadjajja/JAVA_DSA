01879d944be37327ea54c2f738a5bde3fd4c736c
Ahmad Jajja <86593662+Ahmadjajja@users.noreply.github.com>
2025-04-25 12:22:35 -0600

    Time: 0 ms (100.00%) | Memory: 17.7 MB (82.84%) - LeetSync

--- tree/main/DailyLeetcoding/198-house-robber/house-robber.py
+++ tree/main/DailyLeetcoding/198-house-robber/house-robber.py
@@ -1,24 +1,12 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        dp = [-1] * len(nums)
-        maxM = 0
+        nums.append(0)
+        nums.append(0)
 
-        def maxMoney(index):
-            if index == len(nums) - 1 or index == len(nums) - 2:
-                dp[index] = nums[index]
-                return nums[index]
+        [2,7,9,3,1],0,0
 
-            if dp[index] != -1:
-                return dp[index]
-
-            sumM = 0
-            for i in range(index + 2, len(nums)):
-                sumM = max(sumM, nums[index] + maxMoney(i))
-
-            dp[index] = sumM
-            return sumM
-
-        for i in range(len(nums)):
-            maxM = max(maxM, dp[i] if dp[i] != -1 else maxMoney(i))
-
-        return maxM
+        for i in range(len(nums) - 4, -1, -1):
+            nums[i] += max(nums[i + 2], nums[i + 3])
+        
+        return max(nums[0], nums[1])
+        
