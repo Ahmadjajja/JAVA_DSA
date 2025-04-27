41f2e107c06827a752d247ecb9558567b03346d3
Ahmad Jajja <86593662+Ahmadjajja@users.noreply.github.com>
2025-04-27 14:55:44 -0600

    Time: 0 ms (100.00%) | Memory: 17.9 MB (16.23%) - LeetSync

--- tree/main/DailyLeetcoding/213-house-robber-ii/house-robber-ii.py
+++ tree/main/DailyLeetcoding/213-house-robber-ii/house-robber-ii.py
@@ -1,15 +1,17 @@
+from typing import List
+
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        def helper(houses):
-            rob1, rob2 = 0, 0
-
-            # [rob1, rob2, n, n + 1, ...]
-            for n in houses:
-                temp = max(rob1 + n, rob2)
-                rob1 = rob2
-                rob2 = temp
-
-            return rob2
-
-        return max(nums[0], helper(nums[1:]), helper(nums[:-1]))
+        if len(nums) == 1:
+            return nums[0]
+        if len(nums) == 2:
+            return max(nums)
         
+        def rob_linear(arr: List[int]) -> int:
+            arr = arr[:]  # make a copy to avoid modifying original
+            arr.extend([0, 0])
+            for i in range(len(arr) - 3, -1, -1):
+                arr[i] = max(arr[i] + arr[i+2], arr[i+1])
+            return arr[0]
+        
+        return max(rob_linear(nums[:-1]), rob_linear(nums[1:]))
