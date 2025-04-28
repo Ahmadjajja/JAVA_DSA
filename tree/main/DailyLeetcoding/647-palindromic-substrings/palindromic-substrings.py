a7c4b378d0bd770d545491335ed036b53635500d
Ahmad Jajja <86593662+Ahmadjajja@users.noreply.github.com>
2025-04-28 01:11:28 -0600

    Time: 130 ms (63.89%) | Memory: 17.7 MB (66.42%) - LeetSync

---
+++ tree/main/DailyLeetcoding/647-palindromic-substrings/palindromic-substrings.py
@@ -0,0 +1,22 @@
+class Solution:
+    def countSubstrings(self, st: str) -> int:
+        totalPalSubStr = 0
+
+        for i in range(len(st)):
+            # Odd length palindromes
+            l, r = i, i
+            while l >= 0 and r < len(st) and st[l] == st[r]:
+                totalPalSubStr += 1
+                l -= 1
+                r += 1
+
+            # Even length palindromes
+            l, r = i, i + 1
+            while l >= 0 and r < len(st) and st[l] == st[r]:
+                totalPalSubStr += 1
+                l -= 1
+                r += 1
+
+        return totalPalSubStr
+
+        
