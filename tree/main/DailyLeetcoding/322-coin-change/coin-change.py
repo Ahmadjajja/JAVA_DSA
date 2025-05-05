0ac23e5a9538633c28cc347f0bad1ae2cea5fa2b
Ahmad Jajja <86593662+Ahmadjajja@users.noreply.github.com>
2025-05-04 20:07:13 -0600

    Time: 761 ms (55.61%) | Memory: 18.3 MB (43.09%) - LeetSync

--- tree/main/DailyLeetcoding/322-coin-change/coin-change.py
+++ tree/main/DailyLeetcoding/322-coin-change/coin-change.py
@@ -1,44 +1,44 @@
 class Solution:
+#     def coinChange(self, coins: List[int], amount: int) -> int:
+#         memo = {}
+
+#         def helper(remainingAmount):
+
+#             if remainingAmount in memo:
+#                 return memo[remainingAmount]
+#             if remainingAmount == 0:
+#                 return 0
+#             if remainingAmount < 0:
+#                 return -1
+
+#             minCoin = amount + 1
+#             for i in range(len(coins)):
+#                 res = helper(remainingAmount - coins[i])
+#                 if res != -1:
+#                     minCoin = min(minCoin, res + 1)
+
+#             memo[remainingAmount] = minCoin if minCoin != amount + 1 else -1
+#             return memo[remainingAmount]
+
+#         return helper(amount)
+
+
+
+
+
+
+
     def coinChange(self, coins: List[int], amount: int) -> int:
-        memo = {}
+        dp = [amount + 1] *  (amount + 1)
 
-        def helper(remainingAmount):
-
-            if remainingAmount in memo:
-                return memo[remainingAmount]
-            if remainingAmount == 0:
-                return 0
-            if remainingAmount < 0:
-                return -1
-
-            minCoin = amount + 1
-            for i in range(len(coins)):
-                res = helper(remainingAmount - coins[i])
-                if res != -1:
-                    minCoin = min(minCoin, res + 1)
-
-            memo[remainingAmount] = minCoin if minCoin != amount + 1 else -1
-            return memo[remainingAmount]
-
-        return helper(amount)
-
-
-
-
-
-
-
-    # def coinChange(self, coins: List[int], amount: int) -> int:
-    #     dp = [amount + 1] *  (amount + 1)
-
-    #     dp[0] = 0
+        dp[0] = 0
         
-    #     for a in range(1, amount + 1):
-    #         for c in coins:
-    #             if a - c >= 0:
-    #                 dp[a] = min(dp[a], 1 + dp[a - c])
+        for a in range(1, amount + 1):
+            for c in coins:
+                if a - c >= 0:
+                    dp[a] = min(dp[a], 1 + dp[a - c])
 
-    #     return dp[amount] if dp[amount] != amount + 1 else -1
+        return dp[amount] if dp[amount] != amount + 1 else -1
 
 
 
