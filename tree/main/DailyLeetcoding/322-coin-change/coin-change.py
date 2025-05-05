d08f126267f9cc9b600ed0016fceca68c9a5bcb6
Ahmad Jajja <86593662+Ahmadjajja@users.noreply.github.com>
2025-05-04 20:06:01 -0600

    Time: 1006 ms (25.23%) | Memory: 47.5 MB (5.11%) - LeetSync

---
+++ tree/main/DailyLeetcoding/322-coin-change/coin-change.py
@@ -0,0 +1,45 @@
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        memo = {}
+
+        def helper(remainingAmount):
+            if remainingAmount == 0:
+                memo[remainingAmount] = 0
+                return 0
+            if remainingAmount < 0:
+                return -1
+            minCoin = amount + 1
+            for i in range(len(coins)):
+                if (remainingAmount - coins[i]) not in memo:
+                    res = helper(remainingAmount - coins[i])
+                else:
+                    res = memo[remainingAmount - coins[i]]
+                    
+                if res != -1:
+                    minCoin = min(minCoin, res + 1)
+            memo[remainingAmount] = minCoin if minCoin != amount + 1 else -1
+            return memo[remainingAmount]
+
+        return helper(amount)
+
+
+
+
+
+
+
+    # def coinChange(self, coins: List[int], amount: int) -> int:
+    #     dp = [amount + 1] *  (amount + 1)
+
+    #     dp[0] = 0
+        
+    #     for a in range(1, amount + 1):
+    #         for c in coins:
+    #             if a - c >= 0:
+    #                 dp[a] = min(dp[a], 1 + dp[a - c])
+
+    #     return dp[amount] if dp[amount] != amount + 1 else -1
+
+
+
+        
