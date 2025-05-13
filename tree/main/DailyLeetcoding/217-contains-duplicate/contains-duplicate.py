a4b52d1b7a53e4c95b95ce949d7b913de1f66113
Ahmad Jajja <86593662+Ahmadjajja@users.noreply.github.com>
2025-05-12 21:27:21 -0600

    Time: 8 ms (77.73%) | Memory: 31.6 MB (29.72%) - LeetSync

--- tree/main/DailyLeetcoding/217-contains-duplicate/contains-duplicate.py
+++ tree/main/DailyLeetcoding/217-contains-duplicate/contains-duplicate.py
@@ -1,36 +1,14 @@
 class Solution:
-
     def containsDuplicate(self, nums: List[int]) -> bool:
-        # optimize approach
-        hashSet = set()
+        # 1. empty list
+        # 2. for loop
+        # 3. 
+        
+        hashmap = set()
 
-        for num in nums:
-            if num in hashSet:
+        for n in nums:
+            if n in hashmap:
                 return True
-            else:
-                hashSet.add(num)
+            hashmap.add(n)
+        
         return False
-
-
-
-
-        # # brute-force approach
-        # for i in range(0, len(nums)):
-        #     for j in range(0, len(nums)):
-        #         if i != j and nums[i] == nums[j]:
-        #             return True
-        # return False
-
-
-
-
-
-
-# # constraint-1
-# minimum length of list can be 1.
-# maximum length of list can be 100000
-# 1 <= length of list <= 100000
-
-# #constraint-2
-# minimum size of an element of list can be -1000000000
-# maximum size of an element of list can be 1000000000
