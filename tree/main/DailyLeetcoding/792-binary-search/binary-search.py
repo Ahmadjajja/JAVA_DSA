1ad6e131000f9eb8d06fe0958d09c487e45c73cc
Ahmad Jajja <86593662+Ahmadjajja@users.noreply.github.com>
2025-05-16 21:11:31 -0600

    Time: 0 ms (100.00%) | Memory: 18.8 MB (28.18%) - LeetSync

--- tree/main/DailyLeetcoding/792-binary-search/binary-search.py
+++ tree/main/DailyLeetcoding/792-binary-search/binary-search.py
@@ -1,16 +1,10 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        left, right = 0, len(nums) - 1
+        # linear search
 
-        while left <= right:
-            mid = (left + right) // 2
-
-            if target == nums[mid]:
-                return mid
-            elif target < nums[mid]:
-                right = mid - 1
-            else:
-                left = mid + 1
+        for i in range(len(nums)):
+            if nums[i] == target:
+                return i
         
         return -1
-
+        
