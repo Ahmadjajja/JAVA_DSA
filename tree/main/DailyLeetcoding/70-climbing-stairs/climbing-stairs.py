4b5b2f8859d3a3295ab3e3103a789d76f383272d
Ahmad Jajja <86593662+Ahmadjajja@users.noreply.github.com>
2025-05-28 21:33:04 -0600

    Time: 0 ms (100.00%) | Memory: 18 MB (8.00%) - LeetSync

--- tree/main/DailyLeetcoding/70-climbing-stairs/climbing-stairs.py
+++ tree/main/DailyLeetcoding/70-climbing-stairs/climbing-stairs.py
@@ -1,21 +1,25 @@
-# class Solution:
-#     def climbStairs(self, n: int) -> int:
-#         def helper(stepCounter):
-#             if stepCounter == n:
-#                 return 1
-#             if stepCounter > n:
-#                 return 0
-#             return helper(stepCounter + 1) + helper(stepCounter + 2)
-
-#         return helper(0)
-
 class Solution:
     def climbStairs(self, n: int) -> int:
-        one, two = 1, 1
 
-        for i in range(n - 1):
-            temp = one
-            one = one + two
-            two = temp
-        
-        return one
+        memo = {}
+
+        def DFS(steps):
+
+            if steps == n:
+                return 1
+            
+            if steps > n:
+                return 0
+            
+            if steps in memo:
+                return memo[steps]
+            
+            leftAns = DFS(steps + 1)
+
+            rightAns = DFS(steps + 2)
+
+            memo[steps] = leftAns + rightAns
+
+            return memo[steps]
+
+        return DFS(0)
