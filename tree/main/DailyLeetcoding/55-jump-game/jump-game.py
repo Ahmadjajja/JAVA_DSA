374e1cd2f08555e629d2d4b3d056912de83b8fcd
Ahmad Jajja <86593662+Ahmadjajja@users.noreply.github.com>
2025-05-30 22:09:11 -0600

    Time: 39 ms (38.54%) | Memory: 18.6 MB (63.53%) - LeetSync

--- tree/main/DailyLeetcoding/55-jump-game/jump-game.py
+++ tree/main/DailyLeetcoding/55-jump-game/jump-game.py
@@ -1,7 +1,11 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
 
+        # ans = False
+
         # def DFS(index):
+
+        #     nonlocal ans
 
         #     if index == len(nums) - 1:
         #         return True
@@ -9,20 +13,26 @@ class Solution:
         #     if index >= len(nums):
         #         return False
 
-        #     ans = False
 
         #     for i in range(index, index + nums[index]):
 
-        #         ans *= DFS(index + 1)
+        #         ans = ans or DFS(i + 1) 
             
         #     return ans
 
 
         # return DFS(0)
+        maxReach = 0
+        for i in range(len(nums)):
+            if i > maxReach:
+                return False
+            maxReach = max(maxReach, i + nums[i])
+        return True
 
-        goal = len(nums) - 1
-        for i in range(len(nums) - 2, -1, -1):
-            if nums[i] >= (goal - i):
-                goal = i
+
+        # goal = len(nums) - 1
+        # for i in range(len(nums) - 2, -1, -1):
+        #     if nums[i] >= (goal - i):
+        #         goal = i
             
-        return goal == 0
+        # return goal == 0
