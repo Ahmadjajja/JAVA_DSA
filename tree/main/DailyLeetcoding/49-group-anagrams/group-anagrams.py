316106e8759490804b6c620b79a9d9a625d2f4c0
Ahmad Jajja <86593662+Ahmadjajja@users.noreply.github.com>
2025-06-09 22:12:39 -0600

    Time: 71 ms (5.03%) | Memory: 26.7 MB (5.50%) - LeetSync

---
+++ tree/main/DailyLeetcoding/49-group-anagrams/group-anagrams.py
@@ -0,0 +1,28 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+
+        ans = {}
+        for s in strs:
+            tempHM = {}
+            for ch in s:
+                if ch in tempHM:
+                    tempHM[ch] += 1
+                else:
+                    tempHM[ch] = 1
+            
+            print("tempHM : ", tempHM)
+
+            key = tuple(sorted(tempHM.items()))
+            if key in ans:
+                ans[key].append(s)
+            else:
+                ans[key] = [s]
+        
+        print("ans : ", ans)
+
+        return list(ans.values())
+
+
+
+
+        
