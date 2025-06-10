bd26723ff81f1e986439ce1c47af39641d3efa87
Ahmad Jajja <86593662+Ahmadjajja@users.noreply.github.com>
2025-06-09 21:30:50 -0600

    Time: 4 ms (41.46%) | Memory: 19 MB (24.21%) - LeetSync

--- tree/main/DailyLeetcoding/1-two-sum/two-sum.py
+++ tree/main/DailyLeetcoding/1-two-sum/two-sum.py
@@ -2,11 +2,33 @@ class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
 
 
-        for i in range(len(nums)):
-            for j in range(i + 1, len(nums)):
-                if nums[i] + nums[j] == target:
-                    return [i, j]
+        # for i in range(len(nums)):
+        #     for j in range(i + 1, len(nums)):
+        #         if nums[i] + nums[j] == target:
+        #             return [i, j]
 
+        # 1. create a hashmap and store value
+        hashmap = {}
+        # for i in range(len(nums)):
+        #     hashmap[nums[i]] = i
+
+        for i, n in enumerate(nums):
+            if (target - n) in hashmap:
+                return [hashmap[target - n], i]
+            
+            hashmap[n] = i
+
+
+        # 2. 
+        # 3.
 
 # 10^4 * 10^4
         
+# TC = O(n^2)
+# SC = O(1)
+
+
+# O(nlogn)
+
+
+
