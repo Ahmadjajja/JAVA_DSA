005e4fba16391a49db722980bd652ff2ac5b836e
Ahmad Jajja <86593662+Ahmadjajja@users.noreply.github.com>
2025-06-11 23:04:12 -0600

    Time: 246 ms (12.41%) | Memory: 17.9 MB (86.52%) - LeetSync

--- tree/main/DailyLeetcoding/424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
+++ tree/main/DailyLeetcoding/424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
@@ -4,21 +4,23 @@ class Solution:
         i, j = 0, 0
         ans = 0
         fc = [0 for i in range(26)]
+        fc[ord(s[0]) - ord('A')] += 1
 
         while j < len(s):
             twl = j - i + 1 
 
-            fc[ord(s[j]) - ord('A')] += 1
             maxFreqElement = max(fc)
             
             if twl - maxFreqElement <= k:
                 j += 1
                 ans = max(ans, twl)
+                if j == len(s):
+                    break
+                fc[ord(s[j]) - ord('A')] += 1
 
             else:
                 fc[ord(s[i]) - ord('A')] -= 1
                 i += 1
-                j+=1
         
         return ans
         
