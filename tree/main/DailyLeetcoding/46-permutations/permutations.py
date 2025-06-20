31038a5dda8b582d833b5c0eef71817fd129b08d
Ahmad Jajja <86593662+Ahmadjajja@users.noreply.github.com>
2025-06-19 22:55:20 -0600

    Time: 0 ms (100.00%) | Memory: 18 MB (33.45%) - LeetSync

--- tree/main/DailyLeetcoding/46-permutations/permutations.py
+++ tree/main/DailyLeetcoding/46-permutations/permutations.py
@@ -7,21 +7,21 @@ class Solution:
 
             # base case
             if len(per) == len(nums):
-
                 res.append(per.copy())
-
                 return
 
             for i in range(len(nums)):
-                if nums[i] in per:
+                if i in seen:
                     continue
-
+                
+                seen.add(i)
                 per.append(nums[i])
 
                 DFS(per, seen)
 
+                seen.remove(i)
                 per.pop()
-        
+
         DFS([], set())
 
         return res
