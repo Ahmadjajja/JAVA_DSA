00534da541c0b64bf583c57172b0959c4ba873e6
Ahmad Jajja <86593662+Ahmadjajja@users.noreply.github.com>
2025-06-26 22:45:03 -0600

    Time: 0 ms (100.00%) | Memory: 17.9 MB (17.26%) - LeetSync

--- tree/main/DailyLeetcoding/213-house-robber-ii/house-robber-ii.py
+++ tree/main/DailyLeetcoding/213-house-robber-ii/house-robber-ii.py
@@ -1,17 +1,32 @@
 from typing import List
 
 class Solution:
-    def rob(self, nums: List[int]) -> int:
-        if len(nums) == 1:
-            return nums[0]
-        if len(nums) == 2:
-            return max(nums)
-        
-        def rob_linear(arr: List[int]) -> int:
-            arr = arr[:]  # make a copy to avoid modifying original
-            arr.extend([0, 0])
-            for i in range(len(arr) - 3, -1, -1):
-                arr[i] = max(arr[i] + arr[i+2], arr[i+1])
-            return arr[0]
-        
-        return max(rob_linear(nums[:-1]), rob_linear(nums[1:]))
+    def rob(self, numbers: List[int]) -> int:
+        length = len(numbers)
+        if length == 1 or length == 2 or length == 3:
+                return max(numbers)
+
+        def helper(nums):
+
+            memo = {}
+
+            def DFS(index):
+
+                if index >= len(nums):
+                    return 0
+                
+                if index in memo:
+                    return memo[index]
+                
+                left = DFS(index + 2)
+                right = DFS(index + 3)
+
+                ans = max(left, right) + nums[index]
+                memo[index] = ans
+
+                return ans
+
+            return max(DFS(0), DFS(1))
+
+        return max(helper(numbers[:length - 1]), helper(numbers[1:]))
+
