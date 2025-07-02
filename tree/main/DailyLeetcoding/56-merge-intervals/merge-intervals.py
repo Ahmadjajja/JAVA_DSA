29907ba591e7d7f9ebdc21808a0461081d34294c
Ahmad Jajja <86593662+Ahmadjajja@users.noreply.github.com>
2025-07-01 22:19:07 -0600

    Time: 11 ms (30.13%) | Memory: 21.8 MB (14.78%) - LeetSync

--- tree/main/DailyLeetcoding/56-merge-intervals/merge-intervals.py
+++ tree/main/DailyLeetcoding/56-merge-intervals/merge-intervals.py
@@ -6,11 +6,11 @@ class Solution:
 
         for start, end in intervals[1:]:
 
-            if start > ans[len(ans) - 1][1]:
+            if start > ans[-1][1]:
                 ans.append([start, end])
             else:
-                maxV = max(end, ans[len(ans) - 1][1])
-                ans[len(ans) - 1][1] = maxV
+                maxV = max(end, ans[-1][1])
+                ans[-1][1] = maxV
 
         return ans
 
