5587cda815ad58b083426405291cb9364461d251
Ahmad Jajja <86593662+Ahmadjajja@users.noreply.github.com>
2025-07-02 22:08:53 -0600

    Time: 3 ms (69.56%) | Memory: 18.7 MB (16.61%) - LeetSync

--- tree/main/DailyLeetcoding/73-set-matrix-zeroes/set-matrix-zeroes.py
+++ tree/main/DailyLeetcoding/73-set-matrix-zeroes/set-matrix-zeroes.py
@@ -12,15 +12,16 @@ class Solution:
             for col in range(cols):
                 if matrix[row][col] == 0:
                     zerosIndices.append([row, col])
+                    n = [row, col]
 
         for n in zerosIndices:
             # make row to zeros...
             for row in range(rows):
-                if row != n[0]:
+                if row != n[0] and matrix[row][n[1]] != 0:
                     matrix[row][n[1]] = 0
             # make col to zeros...
             for col in range(cols):
-                if col != n[1]:
+                if col != n[1]and matrix[n[0]][col] != 0:
                     matrix[n[0]][col] = 0
 
 
