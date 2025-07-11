0eb3b5e53325fb35f2d6715a71c668d174ac618f
Ahmad Jajja <86593662+Ahmadjajja@users.noreply.github.com>
2025-07-10 21:53:12 -0600

    Time: 15 ms (85.62%) | Memory: 17.8 MB (62.43%) - LeetSync

--- tree/main/DailyLeetcoding/567-permutation-in-string/permutation-in-string.py
+++ tree/main/DailyLeetcoding/567-permutation-in-string/permutation-in-string.py
@@ -1,24 +1,33 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
+        if len(s1) > len(s2):
+            return False
+            
         alphFre = [0] * 26
+        subFre = [0] * 26
 
-        for ch in s1:
-            index = 97 - ord(ch)
-            alphFre[index] += 1
+        for i in range(len(s1)):
+            index1 = ord(s1[i]) - 97 
+            index2 = ord(s2[i]) - 97 
+            alphFre[index1] += 1
+            subFre[index2] += 1
+
         
         l = 0
-        r = len(s1)
+        r = len(s1) 
 
         while r <= len(s2):
-            subFre = [0] * 26
-            subString = s2[l:r]
-
-            for ch in subString:
-                index = 97 - ord(ch)
-                subFre[index] += 1
-            
+            # print("alphFre : ", alphFre)
+            # print("subFre : ", subFre)
+            # print("subStr : ", s2)
             if subFre == alphFre:
                 return True
+            if r < len(s2):
+                indexRight = ord(s2[r]) - 97 
+                indexLeft = ord(s2[l]) - 97
+            
+                subFre[indexRight] += 1
+                subFre[indexLeft] -= 1
             
             l += 1
             r += 1
