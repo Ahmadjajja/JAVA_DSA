9b305a1b3bb697a041642ddcb0b7d5d8b8a73865
Ahmad Jajja <86593662+Ahmadjajja@users.noreply.github.com>
2025-07-17 22:10:03 -0600

    Time: 50 ms (92.56%) | Memory: 20.9 MB (98.06%) - LeetSync

--- tree/main/DailyLeetcoding/235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.py
+++ tree/main/DailyLeetcoding/235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.py
@@ -8,36 +8,12 @@
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
 
-
-
-        def DFS(node):
-
-            if not node:
-                return [False, None]
-
+        while root:
+            if root.val > p.val and root.val > q.val:
+                root = root.left
+            elif root.val < p.val and root.val < q.val:
+                root = root.right
+            else:
+                return root
             
-            
-            left = DFS(node.left)
-            right = DFS(node.right)
-
-            if left[0] and left[1]:
-                return left
-            
-            if right[0] and right[1]:
-                return right
-
-            # case 1
-            if left[0] and right[0]:
-                return [True, node]
-
-
-            # case 2
-            if node.val == p.val or node.val == q.val:
-                if left[0] or right[0]:
-                    return [True, node]
-            
-
-            return [node.val == p.val or node.val == q.val or left[0] or right[0], None]
-        
-        return DFS(root)[1]
         
