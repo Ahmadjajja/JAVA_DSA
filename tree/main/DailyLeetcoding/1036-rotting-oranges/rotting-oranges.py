27a161221ac90a2c56c190f6bd3df13a79d9d5cb
Ahmad Jajja <86593662+Ahmadjajja@users.noreply.github.com>
2025-07-22 21:59:14 -0600

    Time: 6 ms (29.51%) | Memory: 17.9 MB (44.57%) - LeetSync

--- tree/main/DailyLeetcoding/1036-rotting-oranges/rotting-oranges.py
+++ tree/main/DailyLeetcoding/1036-rotting-oranges/rotting-oranges.py
@@ -1,31 +1,33 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        q = deque()
-        time, fresh = 0, 0
+        FOC = 0
+        minMin = 0
 
-        ROWS, COLS = len(grid), len(grid[0])
+        q = collections.deque()
 
-        for r in range(ROWS):
-            for c in range(COLS):
-                if grid[r][c] == 1:
-                    fresh += 1
+        rows, cols = len(grid), len(grid[0])
+        directions = [[0, 1], [1, 0], [0, -1], [-1, 0]]
+
+        for r in range(rows):
+            for c in range(cols):
                 if grid[r][c] == 2:
                     q.append([r, c])
+                elif grid[r][c] == 1:
+                    FOC += 1
         
-        directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
-        while q and fresh > 0:
-            for i in range(len(q)):
+        while q and FOC > 0:
+            sizeQ = len(q)
+            for i in range(sizeQ):
                 r, c = q.popleft()
                 for dr, dc in directions:
-                    row, col = dr + r, dc + c
-                    # if in bounds and fresh, make rotten
-                    if (row< 0 or row == len(grid) or
-                        col < 0 or col == len(grid[0]) or
-                        grid[row][col] != 1):
-                        continue
-                    grid[row][col] = 2
-                    q.append([row, col])
-                    fresh -= 1
-            time += 1
-        return time if fresh == 0 else -1
+                    nr, nc = r + dr, c + dc
+
+                    if 0 <= nr < rows and 0 <= nc < cols and grid[nr][nc] == 1:
+                        q.append([nr, nc])
+                        grid[nr][nc] = 2
+                        FOC -= 1
+            minMin += 1
+        
+        return -1 if FOC != 0 else minMin
+
         
