a4a7b523da77f064f70f250a738b1cdbb5fd3ae0
Ahmad Jajja <86593662+Ahmadjajja@users.noreply.github.com>
2025-09-18 20:47:59 -0600

    Time: 0 ms (100.00%) | Memory: 17.6 MB (78.37%) - LeetSync

--- tree/main/DailyLeetcoding/70-climbing-stairs/climbing-stairs.py
+++ tree/main/DailyLeetcoding/70-climbing-stairs/climbing-stairs.py
@@ -1,37 +1,13 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
+        if n == 1:
+            return 1
+        first, second = 1, 2
 
-        # memo = {}
+        for i in range(2, n):
+            temp = first
+            first = second
+            second = temp + second
 
-        # def DFS(steps):
-
-        #     if steps == n:
-        #         return 1
-            
-        #     if steps > n:
-        #         return 0
-            
-        #     if steps in memo:
-        #         return memo[steps]
-            
-        #     leftAns = DFS(steps + 1)
-
-        #     rightAns = DFS(steps + 2)
-
-        #     memo[steps] = leftAns + rightAns
-
-        #     return memo[steps]
-
-        # return DFS(0)
-
-        two, one = 1, 1
-        n = n - 2
-
-        while n > -1:
-            temp = one
-            one = one + two
-            two = temp
-            n -= 1
+        return second
         
-        return one
-
