e74a49a6c3f53ba588a11181c8b25cd27e33bf7f
Ahmad Jajja <86593662+Ahmadjajja@users.noreply.github.com>
2025-09-22 21:02:41 -0600

    Time: 9 ms (1.49%) | Memory: 17.8 MB (53.91%) - LeetSync

--- tree/main/DailyLeetcoding/198-house-robber/house-robber.py
+++ tree/main/DailyLeetcoding/198-house-robber/house-robber.py
@@ -1,27 +1,28 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        if len(nums) == 1 or len(nums) == 2:
-            return max(nums)
 
         memo = {}
-
-        def DFS(index):
-            if index >= len(nums):
+        def dfs(node):
+            # base case
+            if node > len(nums):
                 return 0
             
-            if index in memo:
-                return memo[index]
+            if (node, nums[node]) in memo:
+                return memo[(node, nums[node])]
+
+            prevMaxVal = 0
+            for i in range(node + 2, len(nums)):
+                prevMaxVal = max(dfs(i), prevMaxVal)
             
-            left = DFS(index + 2)
-            right = DFS(index + 3)
+            memo[(node, nums[node])] = prevMaxVal + nums[node]
 
-            ans = max(left, right) + nums[index]
-            memo[index] = ans
-
-            return ans
-
-
-
+            return prevMaxVal + nums[node]
+            
+        ans = 0
+        for i in range(len(nums)):
+            ans = max(ans, dfs(i))
         
-        return max(DFS(0), DFS(1))
+        return ans
+        
+
         
