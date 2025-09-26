dbdde7ac78983a81f9bdc1f23a8d895c6f412950
Ahmad Jajja <86593662+Ahmadjajja@users.noreply.github.com>
2025-09-26 16:43:50 -0600

    Time: 0 ms (100.00%) | Memory: 19.4 MB (5.62%) - LeetSync

--- tree/main/DailyLeetcoding/1-two-sum/two-sum.py
+++ tree/main/DailyLeetcoding/1-two-sum/two-sum.py
@@ -10,28 +10,40 @@ class Solution:
         
         # return [-1, -1]
 
-        # optimized approach
-        # 2. TC: O(nlogn + n + n) = O(nlogn), SC: O(n)
-        sortedNums = sorted(nums)
-        num1, num2 = 0,  0
+        # # optimized approach
+        # # 2. TC: O(nlogn + n + n) = O(nlogn), SC: O(n)
+        # sortedNums = sorted(nums)
+        # num1, num2 = 0,  0
 
-        s, e = 0, len(nums) - 1
-        while s <= e:
+        # s, e = 0, len(nums) - 1
+        # while s <= e:
 
-            if sortedNums[s] + sortedNums[e] > target:
-                e -= 1
-            elif sortedNums[s] + sortedNums[e] < target:
-                s += 1
-            else:
-                num1 = sortedNums[s]
-                num2 = sortedNums[e]
-                break
+        #     if sortedNums[s] + sortedNums[e] > target:
+        #         e -= 1
+        #     elif sortedNums[s] + sortedNums[e] < target:
+        #         s += 1
+        #     else:
+        #         num1 = sortedNums[s]
+        #         num2 = sortedNums[e]
+        #         break
 
-        ans = []
-        for i in range(len(nums)):
-            if nums[i] == num1 or nums[i] == num2:
-                ans.append(i)
-            if len(ans) == 2:
-                break
+        # ans = []
+        # for i in range(len(nums)):
+        #     if nums[i] == num1 or nums[i] == num2:
+        #         ans.append(i)
+        #     if len(ans) == 2:
+        #         break
         
-        return ans
+        # return ans
+
+        # even better approach
+        hm = {}
+
+        for i in range(len(nums)):
+            diff = target - nums[i]
+            if nums[i] in hm:
+                return [hm[nums[i]], i]
+            hm[diff] = i
+
+
+        
