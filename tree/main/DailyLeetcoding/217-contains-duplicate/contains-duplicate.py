52cd9772d3dfd99150798c8db9752ff3afa5a4f0
Ahmad Jajja <86593662+Ahmadjajja@users.noreply.github.com>
2025-09-26 15:16:28 -0600

    Time: 56 ms (5.15%) | Memory: 26.7 MB (97.33%) - LeetSync

--- tree/main/DailyLeetcoding/217-contains-duplicate/contains-duplicate.py
+++ tree/main/DailyLeetcoding/217-contains-duplicate/contains-duplicate.py
@@ -1,22 +1,22 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        # 1. TC: O(n), SC: O(n)
+        # # 1. TC: O(n), SC: O(n)
 
-        uniqueNums = set()
-        for num in nums:
-            if num in uniqueNums:
-                return True
+        # uniqueNums = set()
+        # for num in nums:
+        #     if num in uniqueNums:
+        #         return True
             
-            uniqueNums.add(num)
+        #     uniqueNums.add(num)
         
-        return False
+        # return False
 
         # 2. 
 
         if len(nums) == 1:
             return False
 
-        Arrays.sort()
+        nums.sort()
 
         for i in range(1, len(nums)):
             if nums[i - 1] == nums[i]:
