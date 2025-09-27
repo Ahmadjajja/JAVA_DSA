6ceba1684f6601b0b5f8441e7367e751a902f93a
Ahmad Jajja <86593662+Ahmadjajja@users.noreply.github.com>
2025-09-27 07:50:51 -0600

    Time: 24 ms (13.00%) | Memory: 22.5 MB (35.18%) - LeetSync

--- tree/main/DailyLeetcoding/49-group-anagrams/group-anagrams.py
+++ tree/main/DailyLeetcoding/49-group-anagrams/group-anagrams.py
@@ -1,28 +1,17 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
 
-        ans = {}
+        groupAnagrams = {}
+
         for s in strs:
-            tempHM = {}
+            uniqueChArrCount = [0] * 26
             for ch in s:
-                if ch in tempHM:
-                    tempHM[ch] += 1
-                else:
-                    tempHM[ch] = 1
-
-            key = tuple(sorted(tempHM.items()))
-            if key in ans:
-                ans[key].append(s)
+                uniqueChArrCount[ord(ch) - ord('a')] += 1
+            tupKey = tuple(uniqueChArrCount)
+            if tupKey in groupAnagrams:
+                groupAnagrams[tupKey].append(s)
             else:
-                ans[key] = [s]
+                groupAnagrams[tupKey] = [s]
         
-
-        return list(ans.values())
-
-# TC = O(N) * k log(k)
-# SC = O(N) * O(k)
-
-
-
-
+        return list(groupAnagrams.values())
         
