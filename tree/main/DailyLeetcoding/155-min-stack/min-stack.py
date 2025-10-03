0fe08c354cbc1b3c069644fc20eb144f1897f9c7
Ahmad Jajja <86593662+Ahmadjajja@users.noreply.github.com>
2025-10-03 07:54:12 -0600

    Time: 17 ms (11.79%) | Memory: 21.3 MB (58.77%) - LeetSync

--- tree/main/DailyLeetcoding/155-min-stack/min-stack.py
+++ tree/main/DailyLeetcoding/155-min-stack/min-stack.py
@@ -2,27 +2,27 @@ class MinStack:
 
     def __init__(self):
         self.stack = []
-        self.minstack = []
+        self.minStack = []
+
+        
 
     def push(self, val: int) -> None:
         self.stack.append(val)
-
-        if not self.minstack or val <= self.minstack[-1]:
-            self.minstack.append(val)
+        val = min(val, self.minStack[-1] if self.minStack else val)
+        self.minStack.append(val)
 
     def pop(self) -> None:
-        popedVal = self.stack.pop()
-
-        if popedVal == self.minstack[-1]:
-            self.minstack.pop()
-
+        self.stack.pop()
+        self.minStack.pop()
         
 
     def top(self) -> int:
         return self.stack[-1]
+        
 
     def getMin(self) -> int:
-        return self.minstack[-1]
+        return self.minStack[-1]
+
         
 
 
