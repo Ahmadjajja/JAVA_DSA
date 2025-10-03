cfccc2982d7676db00f67d6707c316bfa49fcab5
Ahmad Jajja <86593662+Ahmadjajja@users.noreply.github.com>
2025-10-02 21:56:59 -0600

    Time: 1119 ms (19.28%) | Memory: 46 MB (7.64%) - LeetSync

--- tree/main/DailyLeetcoding/322-coin-change/coin-change.py
+++ tree/main/DailyLeetcoding/322-coin-change/coin-change.py
@@ -1,45 +1,29 @@
 class Solution:
-#     def coinChange(self, coins: List[int], amount: int) -> int:
-#         memo = {}
-
-#         def helper(remainingAmount):
-
-#             if remainingAmount in memo:
-#                 return memo[remainingAmount]
-#             if remainingAmount == 0:
-#                 return 0
-#             if remainingAmount < 0:
-#                 return -1
-
-#             minCoin = amount + 1
-#             for i in range(len(coins)):
-#                 res = helper(remainingAmount - coins[i])
-#                 if res != -1:
-#                     minCoin = min(minCoin, res + 1)
-
-#             memo[remainingAmount] = minCoin if minCoin != amount + 1 else -1
-#             return memo[remainingAmount]
-
-#         return helper(amount)
-
-
-
-
-
-
-
     def coinChange(self, coins: List[int], amount: int) -> int:
-        dp = [amount + 1] *  (amount + 1)
+        memo = {}
 
-        dp[0] = 0
+        def dfs(curAmount):
+            if curAmount == 0:
+                return 0
+            
+            if curAmount < 0:
+                return float('inf')
+            
+            if curAmount in memo:
+                return memo[curAmount]
+
+            minCoins = float('inf')
+            for coin in coins:
+                minCoins = min(minCoins, dfs(curAmount - coin))
+            
+            memo[curAmount] = 1 + minCoins
+            
+            return 1 + minCoins
+
+
+        ans = dfs(amount)
+
+        return -1 if ans == float('inf') else ans
         
-        for a in range(1, amount + 1):
-            for c in coins:
-                if a - c >= 0:
-                    dp[a] = min(dp[a], 1 + dp[a - c])
-
-        return dp[amount] if dp[amount] != amount + 1 else -1
-
-
 
         
