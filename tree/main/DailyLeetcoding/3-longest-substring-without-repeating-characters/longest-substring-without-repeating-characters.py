450629aa053cf2ce49affedb2dfc9f43433d7b89
Ahmad Jajja <86593662+Ahmadjajja@users.noreply.github.com>
2025-10-02 18:03:30 -0600

    Time: 19 ms (53.03%) | Memory: 17.8 MB (52.84%) - LeetSync

--- tree/main/DailyLeetcoding/3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ tree/main/DailyLeetcoding/3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -7,19 +7,19 @@ class Solution:
         left, right = 0, 0
 
         while right < len(s):
-            if s[right] in uniqueChs:
-                while s[right] in uniqueChs:
-                    uniqueChs.remove(s[left])
-                    left += 1
-                    count -= 1
-                uniqueChs.add(s[right])
-                count += 1
-            else:
+            if s[right] not in uniqueChs:
                 uniqueChs.add(s[right])
                 count += 1
                 maxCount = max(maxCount, count)
-            
-            right += 1
+                right += 1
+                continue
+            while s[right] in uniqueChs:
+                uniqueChs.remove(s[left])
+                left += 1
+                count -= 1
+            uniqueChs.add(s[right])
+            count += 1
+            right +=1
         
         return maxCount
         
